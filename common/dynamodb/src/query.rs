bad93d0c8c3473a059773ddae469604bc12df794
Anthony Griffon <anthony@griffon.one>
2022-04-29 09:28:40 +0200

    feat: add new query dataloader
    Signed-off-by: Anthony Griffon <anthony@griffon.one>

---
+++ common/dynamodb/src/query.rs
@@ -0,0 +1,140 @@
+use dynomite::{Attribute, AttributeValue, DynamoDbExt};
+use futures_util::TryStreamExt;
+use quick_error::quick_error;
+use rusoto_dynamodb::QueryInput;
+use std::collections::hash_map::Entry;
+use std::collections::HashMap;
+use std::sync::Arc;
+use std::time::Duration;
+
+use crate::dataloader::{DataLoader, Loader, LruCache};
+use crate::DynamoDBContext;
+
+// TODO: Should ensure Rosoto Errors impl clone
+quick_error! {
+    #[derive(Debug, Clone)]
+    pub enum QueryLoaderError {
+        UnknowError {
+            display("An internal error happened")
+        }
+        QueryError {
+            display("An internal error happened while fetching a list of entities")
+        }
+    }
+}
+
+pub struct QueryLoader {
+    ctx: Arc<DynamoDBContext>,
+}
+
+#[derive(PartialEq, Eq, Clone, Hash, Debug)]
+pub struct QueryKey {
+    pub pk: String,
+    pub edges: Vec<String>,
+}
+
+#[async_trait::async_trait]
+impl Loader<QueryKey> for QueryLoader {
+    type Value = HashMap<String, Vec<HashMap<String, AttributeValue>>>;
+    type Error = QueryLoaderError;
+
+    // TODO: We could add a way to select only the edges that we want and split the result based on
+    // the edge.
+    async fn load(&self, keys: &[QueryKey]) -> Result<HashMap<QueryKey, Self::Value>, Self::Error> {
+        log::info!(self.ctx.trace_id, "Query Dataloader invoked {:?}", keys);
+        let mut h = HashMap::new();
+        let mut concurrent_f = vec![];
+        for query_key in keys {
+            let mut exp = dynomite::attr_map! {
+                ":pk" => query_key.pk.clone(),
+            };
+            let edges_len = query_key.edges.len();
+            let sk_string = if edges_len > 0 {
+                Some(
+                    query_key
+                        .edges
+                        .iter()
+                        .enumerate()
+                        .map(|(index, q)| {
+                            exp.insert(format!(":type{}", index), q.clone().into_attr());
+                            format!(" begins_with(#type, :type{})", index)
+                        })
+                        .fold(String::new(), |acc, cur| {
+                            if !acc.is_empty() {
+                                format!("{} OR {}", cur, acc)
+                            } else {
+                                cur
+                            }
+                        }),
+                )
+            } else {
+                None
+            };
+
+            let input: QueryInput = QueryInput {
+                table_name: self.ctx.dynamodb_table_name.clone(),
+                key_condition_expression: Some("#pk = :pk".to_string()),
+                filter_expression: sk_string,
+                expression_attribute_values: Some(exp),
+                expression_attribute_names: Some(HashMap::from([
+                    ("#pk".to_string(), "__pk".to_string()),
+                    ("#type".to_string(), "__type".to_string()),
+                ])),
+
+                ..Default::default()
+            };
+            let future_get = || async move {
+                self.ctx
+                    .dynamodb_client
+                    .clone()
+                    .query_pages(input)
+                    .try_fold(
+                        (query_key.clone(), HashMap::with_capacity(100)),
+                        |(query_key, mut acc), curr| async move {
+                            let partition = curr
+                                .get("__sk")
+                                .and_then(|x| x.s.as_ref())
+                                .and_then(|x| query_key.edges.iter().find(|edge| x.starts_with(edge.as_str())));
+                            match partition {
+                                None => {
+                                    log::error!(self.ctx.trace_id, "Error while processing: {:?}", query_key);
+                                }
+                                Some(partition) => match acc.entry(partition.clone()) {
+                                    Entry::Vacant(vac) => {
+                                        vac.insert(vec![curr]);
+                                    }
+                                    Entry::Occupied(mut old) => {
+                                        old.get_mut().push(curr);
+                                    }
+                                },
+                            }
+                            Ok((query_key, acc))
+                        },
+                    )
+                    .await
+            };
+            concurrent_f.push(future_get());
+        }
+
+        let b = futures_util::future::try_join_all(concurrent_f).await.map_err(|err| {
+            log::error!(self.ctx.trace_id, "Error while querying: {:?}", err);
+            QueryLoaderError::QueryError
+        })?;
+
+        for (q, r) in b {
+            h.insert(q, r);
+        }
+
+        Ok(h)
+    }
+}
+
+pub fn get_loader_query(ctx: Arc<DynamoDBContext>) -> DataLoader<QueryLoader, LruCache> {
+    DataLoader::with_cache(
+        QueryLoader { ctx },
+        wasm_bindgen_futures::spawn_local,
+        LruCache::new(256),
+    )
+    .max_batch_size(10)
+    .delay(Duration::from_millis(2))
+}
