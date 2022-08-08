8b5dbee29a2aee40d736f5d962a1721e2e42fbe0
Yoav Lavi <yoav@yoavlavi.com>
2022-08-08 15:27:02 +0200

    remove request logging

--- cli/crates/server/src/bridge/server.rs
+++ cli/crates/server/src/bridge/server.rs
@@ -81,45 +81,6 @@ async fn mutation_endpoint(
     Ok(StatusCode::OK)
 }
 
-async fn print_request_response(
-    req: Request<Body>,
-    next: Next<Body>,
-) -> Result<impl IntoResponse, (StatusCode, String)> {
-    let (parts, body) = req.into_parts();
-    let bytes = buffer_and_print("request", body).await?;
-    let req = Request::from_parts(parts, Body::from(bytes));
-
-    let res = next.run(req).await;
-
-    let (parts, body) = res.into_parts();
-    let bytes = buffer_and_print("response", body).await?;
-    let res = Response::from_parts(parts, Body::from(bytes));
-
-    Ok(res)
-}
-
-async fn buffer_and_print<B>(direction: &str, body: B) -> Result<Bytes, (StatusCode, String)>
-where
-    B: axum::body::HttpBody<Data = Bytes>,
-    B::Error: std::fmt::Display,
-{
-    let bytes = match hyper::body::to_bytes(body).await {
-        Ok(bytes) => bytes,
-        Err(err) => {
-            return Err((
-                StatusCode::BAD_REQUEST,
-                format!("failed to read {} body: {}", direction, err),
-            ));
-        }
-    };
-
-    if let Ok(body) = std::str::from_utf8(&bytes) {
-        tracing::debug!("{} body = {:?}", direction, body);
-    }
-
-    Ok(bytes)
-}
-
 pub async fn start(port: u16, event_bus: Sender<Event>) -> Result<(), ServerError> {
     trace!("starting bridge at port {port}");
 
@@ -146,8 +107,7 @@ pub async fn start(port: u16, event_bus: Sender<Event>) -> Result<(), ServerErro
         .route("/query", post(query_endpoint))
         .route("/mutation", post(mutation_endpoint))
         .layer(Extension(Arc::clone(&pool)))
-        .layer(TraceLayer::new_for_http())
-        .layer(middleware::from_fn(print_request_response));
+        .layer(TraceLayer::new_for_http());
 
     let socket_address = SocketAddr::from((Ipv4Addr::LOCALHOST, port));
 
