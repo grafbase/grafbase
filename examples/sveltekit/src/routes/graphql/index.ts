6092e793f08dbc412c37fa5b2443285aac546a6d
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2022-08-08 10:40:21 +0200

    Fix TS errors for GRAFBASE_API_KEY env var
    Which might be undefined, but we require it.

--- examples/sveltekit/src/routes/graphql/index.ts
+++ examples/sveltekit/src/routes/graphql/index.ts
@@ -4,11 +4,11 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const POST: RequestHandler = async ({ request }) => {
-  const response = await fetch(process.env.GRAFBASE_API_URL as string, {
+  const response = await fetch(process.env.GRAFBASE_API_URL!, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${process.env.GRAFBASE_API_KEY}`
+      'x-api-key': process.env.GRAFBASE_API_KEY!
     },
     body: JSON.stringify(await request.json())
   }).then((response) => response.json())
