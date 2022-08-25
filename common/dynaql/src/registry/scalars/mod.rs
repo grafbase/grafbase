bddff4029c45bc019dd36553e0ff2718c1bfcbf1
Anthony Griffon <anthony@griffon.one>
2022-08-25 16:32:27 +0200

    feat: add url scalar
    Signed-off-by: Anthony Griffon <anthony@griffon.one>

--- common/dynaql/src/registry/scalars/mod.rs
+++ common/dynaql/src/registry/scalars/mod.rs
@@ -1,6 +1,9 @@
+use std::str::FromStr;
+
 use chrono::{DateTime, Utc};
 use const_format::concatcp;
 use dynaql_value::ConstValue;
+use url::Url;
 
 use crate::{InputValueError, InputValueResult};
 
@@ -14,6 +17,7 @@ pub enum PossibleScalar {
     ID,
     DateTime,
     JSON,
+    Url,
 }
 
 const SPECIFIED_BY_DIRECTIVE: &str = r#"
@@ -27,6 +31,13 @@ A date-time string at UTC, such as 2007-12-03T10:15:30Z, is compliant with the d
 This scalar is a description of an exact instant on the timeline such as the instant that a user account was created.
 """
 scalar DateTime @specifiedBy(url: "https://datatracker.ietf.org/doc/html/rfc3339")
+"#;
+
+const URL_DIRECTIVE: &str = r#"
+"""
+An URL as defined by RFC1738. For example, `https://grafbase.com/foo/` or `mailto:example@grafbase.com`.
+"""
+scalar Url @specifiedBy(url: "http://url.spec.whatwg.org/")
 "#;
 
 const JSON_DIRECTIVE: &str = r#"
@@ -51,7 +62,9 @@ impl PossibleScalar {
                     .map_err(|err| InputValueError::ty_custom("DateTime", err))?;
                 Ok(true)
             }
-            // TODO: Should ensure that a JSON got string key value
+            (Self::Url, ConstValue::String(value)) => Url::from_str(value)
+                .map_err(|err| InputValueError::ty_custom("Url", err))
+                .map(|_| true),
             (Self::JSON, ConstValue::Object(_)) => Ok(true),
             _ => Ok(false),
         }
@@ -64,7 +77,9 @@ impl PossibleScalar {
             '\n',
             DATETIME_DIRECTIVE,
             '\n',
-            JSON_DIRECTIVE
+            JSON_DIRECTIVE,
+            '\n',
+            URL_DIRECTIVE
         )
     }
 }
@@ -86,6 +101,7 @@ impl TryFrom<&str> for PossibleScalar {
             "ID" => Ok(PossibleScalar::ID),
             "DateTime" => Ok(PossibleScalar::DateTime),
             "JSON" => Ok(PossibleScalar::JSON),
+            "Url" => Ok(PossibleScalar::Url),
             _ => Err(PossibleScalarErrors::NotAScalar {
                 expected_ty: value.to_string(),
             }),
@@ -95,9 +111,8 @@ impl TryFrom<&str> for PossibleScalar {
 
 #[cfg(test)]
 mod tests {
-    use dynaql_value::ConstValue;
-
     use super::PossibleScalar;
+    use dynaql_value::ConstValue;
 
     #[test]
     fn check_json_valid() {
@@ -115,6 +130,26 @@ mod tests {
         let const_value = ConstValue::from_json(value).unwrap();
 
         let scalar = PossibleScalar::JSON.check_valid(&const_value);
+        assert!(scalar.is_ok());
+    }
+
+    #[test]
+    fn check_url_valid() {
+        let value = serde_json::Value::String("https://grafbase.com".to_string());
+
+        let const_value = ConstValue::from_json(value).unwrap();
+
+        let scalar = PossibleScalar::Url.check_valid(&const_value);
+        assert!(scalar.is_ok());
+    }
+
+    #[test]
+    fn check_url_mailto_valid() {
+        let value = serde_json::Value::String("mailto:anthony@grafbase.com".to_string());
+
+        let const_value = ConstValue::from_json(value).unwrap();
+
+        let scalar = PossibleScalar::Url.check_valid(&const_value);
         assert!(scalar.is_ok());
     }
 }
