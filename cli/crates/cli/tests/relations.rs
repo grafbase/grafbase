619f5c4d9129391b28773ef841fbc980fb4ac069
Yoav Lavi <yoav@yoavlavi.com>
2022-08-25 19:01:12 +0200

    add tests and cover all SQL / used operation cases (#68)

--- cli/crates/cli/tests/relations.rs
+++ cli/crates/cli/tests/relations.rs
@@ -2,7 +2,8 @@ mod utils;
 
 use serde_json::{json, Value};
 use utils::consts::{
-    RELATIONS_LINK_BLOG_TO_AUTHOR, RELATIONS_MUTATION, RELATIONS_QUERY, RELATIONS_SCHEMA,
+    REALTIONS_LINK_SECONDARY_AUTHOR_TO_BLOG, REALTIONS_RENAME_AUTHOR, RELATIONS_LINK_BLOG_TO_AUTHOR,
+    RELATIONS_MUTATION, RELATIONS_QUERY, RELATIONS_SCHEMA, RELATIONS_UNLINK_AUTHORS_FROM_BLOG,
     RELATIONS_UNLINK_BLOG_FROM_AUTHOR,
 };
 use utils::environment::Environment;
@@ -29,12 +30,13 @@ fn relations() {
     let blog: Value = dot_get!(response, "data.blogCollection.edges.0.node");
     let blog_id: String = dot_get!(blog, "id");
     let first_author_id: String = dot_get!(blog, "authors.0.id");
+    let second_author_id: String = dot_get!(blog, "authors.1.id");
     let first_author_name: String = dot_get!(blog, "authors.0.name");
     let first_authors_blogs: Vec<Value> = dot_get!(response, "data.blogCollection.edges.0.node.authors.0.blogs");
 
     assert!(blog_id.starts_with("blog_"));
     // latest first
-    assert_eq!(first_author_name, "2");
+    assert_eq!(first_author_name, "5");
     assert!(first_authors_blogs.is_empty());
 
     client.gql::<Value>(
@@ -70,4 +72,48 @@ fn relations() {
 
     assert_eq!(current_first_author_id, first_author_id);
     assert!(first_authors_blogs.is_empty());
+
+    client.gql::<Value>(
+        json!({
+            "query": RELATIONS_LINK_BLOG_TO_AUTHOR,
+            "variables": { "id": first_author_id, "blogId": blog_id}
+        })
+        .to_string(),
+    );
+
+    client.gql::<Value>(
+        json!({
+            "query": REALTIONS_LINK_SECONDARY_AUTHOR_TO_BLOG,
+            "variables": { "id": blog_id, "authorId": first_author_id }
+        })
+        .to_string(),
+    );
+
+    let response = client.gql::<Value>(
+        json!({
+            "query": REALTIONS_RENAME_AUTHOR,
+            "variables": { "id": second_author_id, "name": "renamed"  }
+        })
+        .to_string(),
+    );
+
+    let current_author_name: String = dot_get!(response, "data.authorUpdate.author.name");
+
+    assert_eq!(current_author_name, "renamed");
+
+    let response = client.gql::<Value>(
+        json!({
+            "query": RELATIONS_UNLINK_AUTHORS_FROM_BLOG,
+            "variables": {
+                "id": blog_id,
+                "author1": first_author_id,
+                "author2": second_author_id
+            }
+        })
+        .to_string(),
+    );
+
+    let errors: Option<Value> = dot_get_opt!(response, "errors");
+
+    assert!(errors.is_none(), "errors: {:#?}", errors);
 }
