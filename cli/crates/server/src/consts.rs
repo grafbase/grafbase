bba700f242d971d8017d7bd554958f4ed605209c
Yoav Lavi <yoav@yoavlavi.com>
2022-09-12 11:48:05 +0300

    Bump min node version

--- cli/crates/server/src/consts.rs
+++ cli/crates/server/src/consts.rs
@@ -6,4 +6,4 @@ pub const SCHEMA_PARSER_DIR: &str = "parser";
 pub const SCHEMA_PARSER_INDEX: &str = "index.js";
 pub const GIT_IGNORE_FILE: &str = ".gitignore";
 pub const GIT_IGNORE_CONTENTS: &str = "*\n";
-pub const MIN_NODE_VERSION: &str = "v16.7.0";
+pub const MIN_NODE_VERSION: &str = "v16.13.0";
