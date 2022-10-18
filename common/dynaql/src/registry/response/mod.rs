f13d65db56b2bb6ff589ca69b0670850fa5781ba
Anthony Griffon <anthony@griffon.one>
2022-10-18 17:38:22 +0200

    feat: add multiple pattern to construct a response node
    Signed-off-by: Anthony Griffon <anthony@griffon.one>

--- common/dynaql/src/registry/response/mod.rs
+++ common/dynaql/src/registry/response/mod.rs
@@ -24,16 +24,15 @@ use dynaql_value::ConstValue;
 use serde::Serialize;
 use std::fmt::Write;
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, Clone)]
 pub struct QueryResponse<'a> {
     // stats: todo!(),
     /// Root of the whole struct which is a Container
     root: QueryResponseNode<'a>,
 }
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, Clone)]
 pub struct ResponseNode<'a> {
-    parent: Option<&'a QueryResponseNode<'a>>,
     /// If it's a node, it means there is an ID related to it.
     id: &'a str,
     /// Typename of the node
@@ -44,34 +43,31 @@ pub struct ResponseNode<'a> {
     errors: Vec<Error>,
 }
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, Clone)]
 pub struct ResponseContainer<'a> {
-    parent: Option<&'a QueryResponseNode<'a>>,
     /// Children which are (Relation_Name, Node)
     children: Vec<(&'a str, QueryResponseNode<'a>)>,
     /// Errors, not as `ServerError` yet as we do not have the position.
     errors: Vec<Error>,
 }
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, Clone)]
 pub struct ResponseList<'a> {
-    parent: Option<&'a QueryResponseNode<'a>>,
     children: Vec<QueryResponseNode<'a>>,
 }
 
-#[derive(Debug, Serialize)]
-pub struct ResponsePrimitive<'a> {
-    parent: Option<&'a QueryResponseNode<'a>>,
+#[derive(Debug, Serialize, Clone)]
+pub struct ResponsePrimitive {
     value: ConstValue,
 }
 
 /// A Query Response Node
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, Clone)]
 pub enum QueryResponseNode<'a> {
     Node(ResponseNode<'a>),
     Container(ResponseContainer<'a>),
     List(ResponseList<'a>),
-    Primitive(ResponsePrimitive<'a>),
+    Primitive(ResponsePrimitive),
 }
 
 impl<'a> Display for QueryResponseNode<'a> {
@@ -87,6 +83,27 @@ impl<'a> Display for QueryResponseNode<'a> {
     }
 }
 
+impl<'a> QueryResponseNode<'a> {
+    /// Add a child to this node
+    pub fn add_child(&mut self, field: &'a str, child: QueryResponseNode<'a>) {
+        match self {
+            QueryResponseNode::Node(node) => {
+                node.children.push((field, child));
+            }
+            QueryResponseNode::Container(container) => {
+                container.children.push((field, child));
+            }
+            QueryResponseNode::List(_) => {
+                panic!("Can't add a children to a list")
+            }
+            QueryResponseNode::Primitive(_) => {
+                panic!("Can't add a children to a primitive")
+            }
+        }
+    }
+}
+
+/// Key must be a `&str` for now, so it means we'll need to add the escape sequence ourselves
 fn write_object<'a, K: Display + 'a, V: Display + 'a>(
     object: impl IntoIterator<Item = &'a (K, V)>,
     f: &mut Formatter<'_>,
@@ -94,11 +111,11 @@ fn write_object<'a, K: Display + 'a, V: Display + 'a>(
     f.write_char('{')?;
     let mut iter = object.into_iter();
     if let Some((name, value)) = iter.next() {
-        write!(f, "{}: {}", name, value)?;
+        write!(f, "\"{}\":{}", name, value)?;
     }
     for (name, value) in iter {
         f.write_char(',')?;
-        write!(f, "{}: {}", name, value)?;
+        write!(f, "\"{}\":{}", name, value)?;
     }
     f.write_char('}')
 }
@@ -121,15 +138,41 @@ fn write_list<'a, T: Display + 'a>(
 
 #[cfg(test)]
 mod tests {
-    use super::{QueryResponseNode, ResponsePrimitive};
-    use insta::assert_display_snapshot;
+    use super::{QueryResponseNode, ResponseContainer, ResponsePrimitive};
 
     #[test]
     fn should_transform_into_simple_json() {
         let primitive = QueryResponseNode::Primitive(ResponsePrimitive {
-            parent: None,
             value: dynaql_value::ConstValue::String("blbl".into()),
         });
-        assert_display_snapshot!(primitive);
+
+        assert_eq!(primitive.to_string(), serde_json::json!("blbl").to_string());
+    }
+
+    #[test]
+    fn should_transform_example_json() {
+        let mut container = QueryResponseNode::Container(ResponseContainer {
+            errors: vec![],
+            children: vec![],
+        });
+
+        let mut container_title = QueryResponseNode::Container(ResponseContainer {
+            errors: vec![],
+            children: vec![],
+        });
+
+        let primitive = QueryResponseNode::Primitive(ResponsePrimitive {
+            value: dynaql_value::ConstValue::String("example".to_string()),
+        });
+        container_title.add_child("title", primitive);
+        container.add_child("glossary", container_title);
+
+        let output_json = serde_json::json!({
+            "glossary": {
+                "title": "example",
+            }
+        });
+
+        assert_eq!(container.to_string(), output_json.to_string());
     }
 }
