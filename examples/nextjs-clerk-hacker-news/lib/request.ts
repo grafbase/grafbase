d729d0e3b725b58ad6475dcca8b9f0e652316b59
jvidalv <josepvidalvidal@gmail.com>
2022-10-21 19:17:13 +0200

    Fixed missing api key

--- examples/nextjs-clerk-hacker-news/lib/request.ts
+++ examples/nextjs-clerk-hacker-news/lib/request.ts
@@ -1,5 +1,11 @@
 import { GraphQLClient } from 'graphql-request'
 
 export const graphQlRequestClient = new GraphQLClient(
-  process.env.NEXT_PUBLIC_GRAFBASE_API_URL!
+  process.env.NEXT_PUBLIC_GRAFBASE_API_URL!,
+  {
+    headers: {
+      'Content-Type': 'application/json',
+      'x-api-key': process.env.GRAFBASE_API_KEY!
+    }
+  }
 )
