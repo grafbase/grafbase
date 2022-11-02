f5ebdec43c56f1b0e75a377a809056ae9ec3ace5
Jamie Barton <jamie@notrab.dev>
2022-11-02 12:54:11 +0000

    update page index content

--- examples/nextjs-rsc/app/page.tsx
+++ examples/nextjs-rsc/app/page.tsx
@@ -3,16 +3,9 @@ const Page = async () => {
     <>
       <h1>Next.js 13 + Grafbase</h1>
       <p>
-        Here you can select a post from the menu on the left and open it inside
-        of a nested route.
+        Once you've added some posts using the GraphQL Playground, you can
+        explore each post by clicking the link in the nav.
       </p>
-      <h3>You will need to</h3>
-      <ol>
-        <li>
-          Go to <a href="http://localhost:4000">http://localhost:4000</a>
-        </li>
-        <li>Add some new posts with a mutation</li>
-      </ol>
     </>
   )
 }
