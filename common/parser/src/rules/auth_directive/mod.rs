4150595bcf66febdb479a4271073032d0def2fd3
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2022-11-11 15:05:09 +0100

    Extract rules to module

--- common/parser/src/rules/auth_directive/mod.rs
+++ common/parser/src/rules/auth_directive/mod.rs
@@ -1,9 +1,8 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 
 use crate::dynamic_string::DynamicString;
 use crate::rules::model_directive::MODEL_DIRECTIVE;
-
-use super::visitor::{Visitor, VisitorContext};
+use crate::{Visitor, VisitorContext};
 
 use dynaql::{Positioned, ServerError};
 use dynaql_parser::types::ConstDirective;
@@ -13,6 +12,9 @@ use serde::{Deserialize, Serialize};
 
 mod operations;
 use operations::Operations;
+
+mod rules;
+use rules::AuthRule;
 
 const AUTH_DIRECTIVE: &str = "auth";
 const DEFAULT_GROUPS_CLAIM: &str = "groups";
@@ -47,51 +49,6 @@ enum AuthProvider {
 
 fn default_groups_claim() -> String {
     DEFAULT_GROUPS_CLAIM.to_string()
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-#[serde(tag = "allow")]
-#[serde(deny_unknown_fields)]
-#[non_exhaustive]
-enum AuthRule {
-    /// Public data access via API keys
-    // Ex: { allow: anonymous }
-    #[serde(alias = "public")]
-    #[serde(rename_all = "camelCase")]
-    Anonymous {
-        // Note: we don't support operations as our playground needs full access
-    },
-
-    /// Signed-in user data access via OIDC
-    // Ex: { allow: private }
-    //     { allow: private, operations: [create, read] }
-    #[serde(rename_all = "camelCase")]
-    Private {
-        #[serde(default)]
-        operations: Operations,
-    },
-
-    /// User group-based data access via OIDC
-    // Ex: { allow: groups, groups: ["admin"] }
-    //     { allow: groups, groups: ["admin"], operations: [update, delete] }
-    #[serde(rename_all = "camelCase")]
-    Groups {
-        #[serde(with = "::serde_with::rust::sets_duplicate_value_is_error")]
-        groups: HashSet<String>,
-
-        #[serde(default)]
-        operations: Operations,
-    },
-
-    /// Owner-based data access via OIDC
-    // Ex: { allow: owner }
-    //     { allow: owner, operations: [create, read] }
-    #[serde(rename_all = "camelCase")]
-    Owner {
-        #[serde(default)]
-        operations: Operations,
-    },
 }
 
 impl AuthDirective {
@@ -267,24 +224,6 @@ impl AuthProvider {
         }
 
         Ok(provider)
-    }
-}
-
-impl AuthRule {
-    fn from_value(_ctx: &VisitorContext<'_>, value: &ConstValue) -> Result<Self, ServerError> {
-        // We convert the value to JSON to leverage serde for deserialization
-        let value = match value {
-            ConstValue::Object(_) => value
-                .clone()
-                .into_json()
-                .map_err(|err| ServerError::new(err.to_string(), None))?,
-            _ => return Err(ServerError::new("auth rule must be an object", None)),
-        };
-
-        let rule: AuthRule =
-            serde_json::from_value(value).map_err(|err| ServerError::new(format!("auth rule: {err}"), None))?;
-
-        Ok(rule)
     }
 }
 
