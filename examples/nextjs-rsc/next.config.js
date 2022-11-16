f939adb83c143f8edf2ff14391fc9d8b343542aa
Jamie Barton <jamie@notrab.dev>
2022-11-16 14:17:56 +0000

    fix(nextjs-rsc): use experimental edge runtime

--- examples/nextjs-rsc/next.config.js
+++ examples/nextjs-rsc/next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: { appDir: true }
+  experimental: {
+    appDir: true,
+    runtime: 'experimental-edge'
+  }
 }
 
 module.exports = nextConfig
