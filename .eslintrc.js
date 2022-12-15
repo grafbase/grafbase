0668d612a3abf1af40d5c3cb0334cfae7a636fea
Vitaly <9862028+vitalikda@users.noreply.github.com>
2022-12-15 14:04:04 +0300

    update .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,4 @@
 module.exports = {
   root: true,
-  // This tells ESLint to load the config from the package `eslint-config-grafbase`
-  extends: ['grafbase'],
-  settings: {
-    next: {
-      rootDir: ['apps/*/']
-    }
-  }
+  extends: ['grafbase']
 }
