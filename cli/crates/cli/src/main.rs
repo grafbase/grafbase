c5ee553b4fc188c262605edd0071274a90f20e92
Yoav Lavi <yoav@yoavlavi.com>
2022-12-19 17:54:27 +0200

    fix

--- cli/crates/cli/src/main.rs
+++ cli/crates/cli/src/main.rs
@@ -44,7 +44,7 @@ fn main() {
 }
 
 fn try_main() -> Result<(), CliError> {
-    return Err(CliError::ServerPanic("".to_owned()));
+    return Err(CliError::ServerPanic(String::new()));
     let matches = build_cli().get_matches();
 
     let filter = EnvFilter::builder().parse_lossy(if let Ok(Some(true)) = matches.try_get_one("trace") {
