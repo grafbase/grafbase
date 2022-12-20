1f7b258504535003f709c8c2c0fb03b4a37bd06c
Jakub Wieczorek <jakub@grafbase.com>
2022-12-20 15:13:28 +0100

    Rename two common package directories to use a dash instead of an underscore.

---
+++ common/graph-entities/src/lib.rs
@@ -0,0 +1,13 @@
+mod response;
+pub use response::{
+    QueryResponse, QueryResponseErrors, QueryResponseNode, RelationOrigin, ResponseContainer, ResponseList,
+    ResponseNodeId, ResponseNodeRelation, ResponsePrimitive,
+};
+
+mod id;
+pub use id::{
+    normalize_constraint_value, ConstraintDefinition, ConstraintID, ConstraintIDError, ConstraintType, IDError, NodeID,
+    NodeIDError, ID,
+};
+
+pub mod cursor;
