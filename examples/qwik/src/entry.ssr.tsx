757418e9450648996f17c78bacdf998424995487
Heather <hhthacker@gmail.com>
2023-01-18 18:02:26 -0600

    cleaned up files, no need for routing

--- examples/qwik/src/entry.ssr.tsx
+++ examples/qwik/src/entry.ssr.tsx
@@ -18,10 +18,5 @@ export default function (opts: RenderToStreamOptions) {
   return renderToStream(<Root />, {
     manifest,
     ...opts,
-    // Use container attributes to set attributes on the html tag.
-    containerAttributes: {
-      lang: 'en-us',
-      ...opts.containerAttributes,
-    },
   });
 }
--- examples/qwik/src/root.tsx
+++ examples/qwik/src/root.tsx
@@ -1,27 +1,16 @@
-import { component$ } from '@builder.io/qwik';
-import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city';
-import { RouterHead } from './components/router-head/router-head';
+import App from './app';
 
-
-export default component$(() => {
-  /**
-   * The root of a QwikCity site always start with the <QwikCityProvider> component,
-   * immediately followed by the document's <head> and <body>.
-   *
-   * Dont remove the `<head>` and `<body>` elements.
-   */
-
+export default () => {
   return (
-    <QwikCityProvider>
+    <>
       <head>
         <meta charSet="utf-8" />
-        <link rel="manifest" href="/manifest.json" />
-        <RouterHead />
+        <title>Qwik and Grafbase</title>
       </head>
-      <body lang="en">
-        <RouterOutlet />
-        <ServiceWorkerRegister />
+      <body>
+        <h1>Messages</h1>
+        <App />
       </body>
-    </QwikCityProvider>
+    </>
   );
-});
+};
