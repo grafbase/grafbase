df67a7f0217949c6d19963095071ae8e6a095cba
Anthony Griffon <anthony@griffon.one>
2023-02-07 02:10:23 +0100

    feat(parser): start linking internal schema to IR
    Signed-off-by: Anthony Griffon <anthony@griffon.one>

--- common/parser/src/rules/model_directive.rs
+++ common/parser/src/rules/model_directive.rs
@@ -189,6 +189,8 @@ impl<'a> Visitor<'a> for ModelDirective {
                 .iter()
                 .filter_map(|field| UniqueDirective::parse(ctx, object, &type_name, field))
                 .collect::<Vec<_>>();
+            // Schema ID
+            let schema_id = ctx.new_schema_id();
 
             // Add typename schema
             let schema_id = ctx.get_schema_id(&type_name);
@@ -387,6 +389,9 @@ impl<'a> Visitor<'a> for ModelDirective {
                 &type_name,
                 &type_name,
             );
+
+            // Add a new schema to build.
+            ctx.add_schema(Some(schema_id), &type_name);
 
             //
             // GENERATE QUERY ONE OF: type(by: { ... })
