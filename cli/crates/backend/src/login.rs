aa1c692d5f8ef776c164b819ae1dc678ec57d236
Yoav Lavi <yoav@yoavlavi.com>
2023-02-10 14:59:06 +0200

    minor comment tweaks

--- cli/crates/backend/src/login.rs
+++ cli/crates/backend/src/login.rs
@@ -74,11 +74,11 @@ struct LoginApiState {
 ///
 /// # Errors
 ///
-/// - returns [`BackendError::FindUserDotGrafbaseFolder`] if the path of '~/.grafbase' could not be found
+/// - returns [`BackendError::FindUserDotGrafbaseFolder`] if the path of ~/.grafbase could not be found
 ///
-/// - returns [`BackendError::CreateUserDotGrafbaseFolder`] if '~/.grafbase' could not be created
+/// - returns [`BackendError::CreateUserDotGrafbaseFolder`] if ~/.grafbase could not be created
 ///
-/// - returns [`BackendError::ReadUserDotGrafbaseFolder`] if '~/.grafbase' could not be read
+/// - returns [`BackendError::ReadUserDotGrafbaseFolder`] if ~/.grafbase could not be read
 ///
 /// - returns [`BackendError::StartLoginServer`] if the login server could not be started
 #[allow(clippy::needless_pass_by_value)] // &Sender is not Sync
