1749100b08dce5455592471a33ea3a515d86fb26
Graeme Coupar <graeme@grafbase.com>
2023-02-21 11:44:19 +0000

    Fetch & parse OpenAPI schemas as part of deployments

--- common/parser-openapi/src/lib.rs
+++ common/parser-openapi/src/lib.rs
@@ -1,19 +1,61 @@
 use graph::OpenApiGraph;
 use openapiv3::OpenAPI;
 use parsing::components::Ref;
+use url::Url;
 
 mod graph;
 mod output;
 mod parsing;
 
-pub fn parse_spec(data: &str) -> String {
-    let spec = serde_json::from_str::<OpenAPI>(data)
-        .map_err(Error::ParsingError)
-        .unwrap();
+pub fn parse_spec(data: &str, format: Format) -> Result<String, Vec<Error>> {
+    let spec = match format {
+        Format::Json => serde_json::from_str::<OpenAPI>(data)
+            .map_err(Error::ParsingError)
+            .unwrap(),
+        Format::Yaml => todo!(),
+    };
 
-    let graph = OpenApiGraph::new(parsing::parse(spec).unwrap());
+    let graph = OpenApiGraph::new(parsing::parse(spec)?);
 
-    output::output(&graph).unwrap()
+    Ok(output::output(&graph).expect("Formatting shouldn't really fail"))
+}
+
+pub enum Format {
+    Json,
+    Yaml,
+}
+
+impl Format {
+    pub fn guess(content_type: Option<&str>, url: &str) -> Self {
+        if let Some(content_type) = content_type {
+            if content_type == "application/json" {
+                return Format::Json;
+            }
+            if content_type.contains("yaml") {
+                return Format::Yaml;
+            }
+        }
+        if let Some(extension) = extract_extension(url) {
+            if extension.eq_ignore_ascii_case("json") {
+                return Format::Json;
+            }
+            if extension.eq_ignore_ascii_case("yml") || extension.eq_ignore_ascii_case("yaml") {
+                return Format::Yaml;
+            }
+        }
+
+        // YAML is a superset of JSON so lets just fallback to parsing as YAML.
+        Format::Yaml
+    }
+}
+
+fn extract_extension(url: &str) -> Option<String> {
+    let url = Url::parse(url).ok()?;
+    let last_segment = url.path_segments()?.last()?;
+
+    let extension = std::path::Path::new(last_segment).extension()?;
+
+    Some(extension.to_str()?.to_string())
 }
 
 #[derive(thiserror::Error, Debug)]
@@ -60,6 +102,6 @@ mod tests {
     fn test_stripe_output() {
         let spec = std::fs::read_to_string("test_data/stripe.openapi.json").unwrap();
 
-        insta::assert_snapshot!(parse_spec(&spec));
+        insta::assert_snapshot!(parse_spec(&spec, Format::Json).unwrap());
     }
 }
