4055ae9afed81c89481bcba059492c06a8155f68
Benjamin Rabier <rabier.benj@gmail.com>
2023-02-27 20:55:48 +0100

    fix(cli/scripts): Simplify load-assets script

--- cli/scripts/load-assets.sh
+++ cli/scripts/load-assets.sh
@@ -6,16 +6,16 @@ set -euo pipefail
 cli_dir="$(dirname "$(dirname "$(realpath "$0")")")"
 cd "$cli_dir"
 
-branch="$(git rev-parse --abbrev-ref HEAD)"
-assets="latest"
-force="" # false
+branch="main"
+ci_assets="$(grep 'ASSETS_VERSION:' "$cli_dir/../.github/workflows/cli.yml" | cut -d'/' -f2 | tr -d \')"
+latest_assets="latest"
+assets=""
 
 show_help() {
 	printf "%s [-b branch] [-a assets] [-f]\n" "$(basename "$0")"
 	printf "  -h/--help   Show this message.\n"
-	printf "  -f/--force  Whether to force the assets download.\n"
 	printf "  -b/--branch Branch of api repo. Defaults to '%s'.\n" "$branch"
-	printf "  -a/--assets Assets version to load. Defaults to '%s'.\n" "$assets"
+	printf "  -a/--assets Assets version to load. Defaults to '%s' ('%s' for 'main' branch).\n" "$latest_assets" "$ci_assets"
 	printf "              See CI upload-cli-assets job, usually '<commit>-<date>'.\n"
 	printf "\n"
 }
@@ -48,9 +48,6 @@ if [[ "$#" -ne 0 ]]; then
 				die "Error: $1 requires a non-empty option argument."
 			fi
 			;;
-		-f | --force)
-			force="true"
-			;;
 		--) # End of all options.
 			shift
 			break
@@ -69,40 +66,33 @@ if [[ "$#" -ne 0 ]]; then
 fi
 
 assets_dir="$cli_dir/crates/server/assets"
-version_file="$assets_dir/.version"
+if [[ -z "$assets" ]]; then
+	if [[ "$branch" == "main" ]]; then
+		printf "Loading latest assets used by the CI...\n"
+		assets=$ci_assets
+	else
+		assets=$latest_assets
+	fi
+fi
 if [[ "$branch" == "main" ]]; then
 	url="https://assets.grafbase.com/cli/release/$assets.zip"
 else
 	url="https://assets.grafbase.dev/cli/$branch/$assets.zip"
 fi
 
-if [[ -f "$version_file" ]]; then
-	current_version="$(cat "$version_file")"
-	printf "Found existing assets: %s\n" "$current_version"
-else
-	current_version=""
-fi
+printf "Cleaning up old assets...\n"
+rm -rf "$HOME/.grafbase"
+rm -rf "$assets_dir"
+mkdir -p "$assets_dir"
+cd "$assets_dir"
 
-if [[ -n "$force" ]] || [[ "$current_version" != "$url" ]]; then
-	printf "Cleaning up old assets...\n"
-	rm -rf "$HOME/.grafbase"
-	rm -rf "$assets_dir"
-	mkdir -p "$assets_dir"
-	cd "$assets_dir"
-
-	printf "Loading new assets: %s...\n" "$url"
-	curl "$url" -o assets.zip
-	unzip assets.zip &>/dev/null
-	rm assets.zip
-	echo -n "$url" >"$version_file"
-
-	printf "Done!\n"
-fi
-
+printf "Loading new assets: %s...\n" "$url"
+curl --fail "$url" -o assets.zip
+unzip assets.zip &>/dev/null
+rm assets.zip
 cat >"$assets_dir/.gitignore" <<EOM
 *
 !.gitignore
 EOM
 
-cd "$cli_dir"
-cargo run -- dev
+printf "Done!\n"
