723b20b732cb49d705ffb9f031194f3760d24e18
Yoav Lavi <yoav@yoavlavi.com>
2023-03-14 20:10:01 +0200

    add todo

--- cli/crates/backend/src/consts.rs
+++ cli/crates/backend/src/consts.rs
@@ -3,8 +3,7 @@ use const_format::formatcp;
 pub const DEFAULT_SCHEMA: &str = include_str!("../assets/default-schema.graphql");
 pub const CREDENTIALS_FILE: &str = "credentials.json";
 pub const PROJECT_METADATA_FILE: &str = "project.json";
+// TODO change these to grafbase.com and provide a way to test with .dev locally
 pub const AUTH_URL: &str = "https://grafbase.dev/auth/cli";
-// TODO remove this
-#[allow(unused)]
 pub const API_URL: &str = "https://api.grafbase.dev/graphql";
 pub const USER_AGENT: &str = formatcp!("Grafbase-CLI-{}", env!("CARGO_PKG_VERSION"));
