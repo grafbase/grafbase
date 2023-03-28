34eee554b7b95ea2e556b70a5eb3fc2e265a94fa
Jamie Barton <jamie@notrab.dev>
2023-03-28 12:40:28 +0100

    update env

--- examples/nextjs-auth0/pages/api/auth/token.ts
+++ examples/nextjs-auth0/pages/api/auth/token.ts
@@ -4,9 +4,7 @@ import { withApiAuthRequired, getAccessToken } from '@auth0/nextjs-auth0'
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { accessToken: token } = await getAccessToken(req, res, {
     authorizationParams: {
-      audience: 'https://grafbase.com',
-      issuerBaseURL: 'https://grafbase-examples.eu.auth0.com/',
-      tokenSigningAlg: 'RS256'
+      audience: 'https://grafbase.com'
     }
   })
 
