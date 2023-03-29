3dfcd72cb66e83625b941aa8576800ec2066be98
Graeme Coupar <graeme@grafbase.com>
2023-03-29 16:47:14 +0100

    Handle another edge case in directive_de.
    You got a very weird error out of serde if you provided an Enum as the
    value of an unknown key without this change.

--- common/parser/src/directive_de.rs
+++ common/parser/src/directive_de.rs
@@ -108,7 +108,7 @@ impl<'de> serde::de::Deserializer<'de> for ValueDeserializer<'de> {
     serde::forward_to_deserialize_any! {
         bool i8 i16 i32 i64 i128 u8 u16 u32 u64 u128 f32 f64 char str string
         bytes byte_buf unit unit_struct newtype_struct seq tuple
-        tuple_struct map struct enum identifier ignored_any
+        tuple_struct map struct enum identifier
     }
 
     fn deserialize_option<V>(self, visitor: V) -> Result<V::Value, Self::Error>
@@ -119,6 +119,13 @@ impl<'de> serde::de::Deserializer<'de> for ValueDeserializer<'de> {
             ConstValue::Null => visitor.visit_none(),
             _ => visitor.visit_some(self),
         }
+    }
+
+    fn deserialize_ignored_any<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        visitor.visit_unit()
     }
 }
 
