730b72758f3b1833ca8215013061f2d3b2f4e416
Graeme Coupar <grambo@grambo.me.uk>
2023-03-29 10:57:45 +0100

    Bump the minimum node version to 18
    The `@openapi` directive work requires NodeJS versions that support
    `fetch`.  This was stable from 18 onwards, so I'm increasing the
    MIN_NODE_VERSION to that so we get warnings when a user is on an older
    version instead of an obscure error.

--- cli/crates/server/src/consts.rs
+++ cli/crates/server/src/consts.rs
@@ -3,5 +3,5 @@ pub const SCHEMA_PARSER_DIR: &str = "parser";
 pub const SCHEMA_PARSER_INDEX: &str = "index.js";
 pub const GIT_IGNORE_FILE: &str = ".gitignore";
 pub const GIT_IGNORE_CONTENTS: &str = "*\n";
-pub const MIN_NODE_VERSION: &str = "v16.13.0";
+pub const MIN_NODE_VERSION: &str = "v18.0.0";
 pub const DOT_ENV_FILE: &str = ".env";
