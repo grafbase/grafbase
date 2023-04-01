223ec5324a07137c93b2ce2918726f70304c2a99
Jakub Wieczorek <jakub@grafbase.com>
2023-04-01 17:51:52 +0200

    Add a few custom resolver tests.

--- cli/crates/server/src/environment.rs
+++ cli/crates/server/src/environment.rs
@@ -9,10 +9,12 @@ pub fn variables() -> impl Iterator<Item = (String, String)> {
     // We don't use dotenv::dotenv() as we don't want to pollute the process' environment.
     // Doing otherwise would make us unable to properly refresh it whenever any of the .env files
     // changes which is something we may want to do in the future.
-    std::env::vars().chain(
-        dotenv::from_path_iter(dot_env_file_path)
-            .into_iter()
-            .flatten()
-            .filter_map(Result::ok),
-    )
+    std::env::vars()
+        .chain(
+            dotenv::from_path_iter(dot_env_file_path)
+                .into_iter()
+                .flatten()
+                .filter_map(Result::ok),
+        )
+        .chain(std::iter::once(("GRAFBASE_ENV".to_string(), "dev".to_string())))
 }
