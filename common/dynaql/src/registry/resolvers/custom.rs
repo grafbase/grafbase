d2ababeed0a0995624d96675f96ea79fe005a012
Anthony Griffon <anthony@griffon.one>
2023-04-06 13:06:00 +0200

    hack(dynaql): allow parent hydratation for old behavior
    Signed-off-by: Anthony Griffon <anthony@griffon.one>

--- common/dynaql/src/registry/resolvers/custom.rs
+++ common/dynaql/src/registry/resolvers/custom.rs
@@ -1,6 +1,8 @@
 use super::{ResolvedValue, ResolverContext, ResolverTrait};
 
 use crate::{Context, Error};
+use dynamodb::attribute_to_value;
+use dynomite::AttributeValue;
 use grafbase_runtime::custom_resolvers::{
     CustomResolverRequest, CustomResolverRequestPayload, CustomResolversEngine,
 };
@@ -21,8 +23,57 @@ impl ResolverTrait for CustomResolver {
         &self,
         ctx: &Context<'_>,
         _resolver_ctx: &ResolverContext<'_>,
-        _last_resolver_value: Option<&ResolvedValue>,
+        last_resolver_value: Option<&ResolvedValue>,
     ) -> Result<ResolvedValue, Error> {
+        // Little hack while QP is not live
+        //
+        // We know the format of the parent value, we then apply some little magic to adapt it to
+        // the proper format expected.
+        let parent_data = last_resolver_value
+            .map(|x| x.data_resolved.clone())
+            .unwrap_or(Arc::new(serde_json::json!({})));
+
+        // We take the first item as the parent has a struct like: { type: Value }.
+        let parent_data = parent_data
+            .as_object()
+            .and_then(|x| x.iter().next())
+            .map(|(_, x)| x.clone())
+            .unwrap_or(serde_json::json!({}));
+
+        // Magic function to convert the dynamodb format to the format we want to have on the
+        // ResolveR.
+        let value = match parent_data {
+            serde_json::Value::Object(val) => {
+                let temp = val
+                    .into_iter()
+                    .filter_map(|(field, val)| {
+                        if field.starts_with('_') {
+                            let new_field = match field.as_str() {
+                                "__created_at" => Some("createdAt"),
+                                "__updated_at" => Some("updatedAt"),
+                                "__sk" => Some("id"),
+                                _ => None,
+                            };
+                            new_field.map(|x| (x.to_string(), val))
+                        } else {
+                            Some((field, val))
+                        }
+                    })
+                    .map(|(field, x)| {
+                        let attribute = serde_json::from_value::<AttributeValue>(x)
+                            .ok()
+                            .unwrap_or_default();
+
+                        (field, attribute_to_value(attribute))
+                    })
+                    .collect();
+                serde_json::Value::Object(temp)
+            }
+            _ => serde_json::json!({}),
+        };
+
+        // -- End of hack
+
         let custom_resolvers_engine = ctx.data::<CustomResolversEngine>()?;
         let arguments = ctx
             .field()
@@ -36,7 +87,7 @@ impl ResolverTrait for CustomResolver {
                 resolver_name: self.resolver_name.clone(),
                 payload: CustomResolverRequestPayload {
                     arguments,
-                    parent: None,
+                    parent: Some(value),
                 },
             },
         ));
