87f2ea4d766d5dac516b3286715e1727dc5188c3
Jakub Wieczorek <jakub@grafbase.com>
2023-04-07 16:38:45 +0200

    Clippy.

--- cli/crates/cli/src/dev.rs
+++ cli/crates/cli/src/dev.rs
@@ -28,7 +28,7 @@ pub fn dev(search: bool, watch: bool, external_port: Option<u16>, tracing: bool)
                 loop {
                     match receiver.recv() {
                         Ok(ServerMessage::Ready(port)) => {
-                            READY.call_once(|| report::start_server(resolvers_reported, port, start_port))
+                            READY.call_once(|| report::start_server(resolvers_reported, port, start_port));
                         }
                         Ok(ServerMessage::Reload(path, file_event_type)) => report::reload(path, file_event_type),
                         Ok(ServerMessage::StartResolverBuild(resolver_name)) => {
