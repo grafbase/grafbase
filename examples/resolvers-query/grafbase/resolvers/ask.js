5fa897e3b4c9e238a645aa5825c485e60bd92b6e
Jamie Barton <jamie@notrab.dev>
2023-04-17 14:32:11 +0200

    update openai response

--- examples/resolvers-query/grafbase/resolvers/ask.js
+++ examples/resolvers-query/grafbase/resolvers/ask.js
@@ -1,12 +1,12 @@
 import { Configuration, OpenAIApi } from 'openai'
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY
-})
-
-const openai = new OpenAIApi(configuration)
-
 export default async function Resolver(_, { prompt }) {
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY
+  })
+
+  const openai = new OpenAIApi(configuration)
+
   const response = await openai.createCompletion({
     model: 'text-davinci-003',
     prompt,
