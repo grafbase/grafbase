dbcec47d18b88295d66fc7f06997173a83b9dba9
Jonathan Brennan <jonathanawesome@users.noreply.github.com>
2023-04-24 06:53:59 -0500

    Gb 3594 strip all tabs related logic from (#1366)
    * remove tab components and conditional to display inside base-ide
    
    * completes removal of tabs
    
    * update removeTargetField logic
    
    * update initial model names to remove count postfix
    
    * remove includeReset from loadSchema function params
    
    * move monaco-graphql initialization from loadSchema to setMonacoGraphQLSchema
    
    * remove remaining uses of "withTabs" Pathfinder prop
    
    * cleanup useOptions types file

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -46,7 +46,6 @@ export const App = () => {
                 protocol: 'EVENT_SOURCE'
               }
             }}
-            withTabs={false}
             withVisualOperationBuilder={true}
           />
         </StyledToolDisplayWrapper>
