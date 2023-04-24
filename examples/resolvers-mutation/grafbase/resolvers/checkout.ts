40acc763439bf062a086dad3d220a8c2aaae03a3
Jamie Barton <jamie@notrab.dev>
2023-04-24 14:43:24 +0100

    update example

--- examples/resolvers-mutation/grafbase/resolvers/checkout.ts
+++ examples/resolvers-mutation/grafbase/resolvers/checkout.ts
@@ -1,15 +1,15 @@
 import Stripe from 'stripe'
 
-const stripe = Stripe(process.env.STRIPE_SECRET_KEY)
-
 export default async function Resolver(_, { input }) {
+  const stripe = Stripe(process.env.STRIPE_SECRET_KEY)
+
   const { lineItems } = input
 
-  const { data } = await stripe.checkout.sessions.create({
+  const { url } = await stripe.checkout.sessions.create({
     success_url: 'https://example.com/success',
     line_items: lineItems,
     mode: 'payment'
   })
 
-  return { url: data.url }
+  return { url }
 }
