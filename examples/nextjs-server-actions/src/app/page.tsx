619401c03ebba1d21a52cad626c0dfd3c744df90
Jamie Barton <jamie@notrab.dev>
2023-05-05 15:23:55 +0100

    update readme

--- examples/nextjs-server-actions/src/app/page.tsx
+++ examples/nextjs-server-actions/src/app/page.tsx
@@ -1,27 +1,6 @@
 import { revalidateTag } from 'next/cache'
-import { cache } from 'react'
 
-const gql = cache(async (query: string) => {
-  const apiUrl = process.env.GRAFBASE_API_URL || 'http://127.0.0.1:4000/graphql'
-  const apiKey = process.env.GRAFBASE_API_KEY || 'letmein'
-
-  const res = await fetch(apiUrl, {
-    headers: {
-      'x-api-key': apiKey
-    },
-    method: 'POST',
-    body: JSON.stringify({
-      query
-    }),
-    next: {
-      tags: [query]
-    }
-  })
-
-  const { data } = await res.json()
-
-  return data
-})
+import { gql } from './grafbase'
 
 export default async function Home() {
   const query = /* GraphQL */ `
