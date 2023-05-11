40f897617048f69b373fbbd79dffc10dd10ffe50
Yoav Lavi <yoav@yoavlavi.com>
2023-05-11 13:03:26 +0300

    GB-3729 - Support passing the access token via an env var (#352)
    * Support passing the access token via an env var
    
    * fix
    
    * version bumps. changelog, touch changelog on bump
    
    * remove unused

--- cli/crates/backend/src/api/consts.rs
+++ cli/crates/backend/src/api/consts.rs
@@ -4,3 +4,4 @@ pub const AUTH_URL: &str = "https://grafbase.com/auth/cli";
 pub const API_URL: &str = "https://api.grafbase.com/graphql";
 pub const PACKAGE_JSON: &str = "package.json";
 pub const TAR_CONTENT_TYPE: &str = "application/x-tar";
+pub const GRAFBASE_ACCESS_TOKEN_ENV_VAR: &str = "GRAFBASE_ACCESS_TOKEN";
