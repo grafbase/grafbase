5b44d533d41030d9aefe2c0df3fdf6128d594e4e
Graeme Coupar <grambo@grambo.me.uk>
2023-07-21 14:53:46 +0100

    feat: add connector transforms to grafbase-sdk (#550)

---
+++ packages/grafbase-sdk/src/connector/openapi/transforms.ts
@@ -0,0 +1,59 @@
+import { SchemaTransforms, SchemaTransform } from '../transforms'
+
+export type OpenApiTransformsGenerator = (schema: OpenApiTransforms) => void
+export type OpenApiTransform = SchemaTransform | QueryNamingTransform
+
+export type OpenApiQueryNamingStrategy = 'OPERATION_ID' | 'SCHEMA_NAME'
+
+/**
+ * An accumulator class to gather transforms for an OpenAPI connector
+ */
+export class OpenApiTransforms {
+  private _schemaTransforms: SchemaTransforms
+  private _queryNaming: OpenApiQueryNamingStrategy | null
+
+  constructor() {
+    this._schemaTransforms = new SchemaTransforms()
+    this._queryNaming = null
+  }
+
+  public get transforms(): OpenApiTransform[] {
+    const transforms: OpenApiTransform[] = this._schemaTransforms.transforms
+    if (this._queryNaming != null) {
+      transforms.push(new QueryNamingTransform(this._queryNaming))
+    }
+    return transforms
+  }
+
+  /**
+   * Sets the query naming strategy for this OpenAPI connector
+   */
+  public queryNaming(strategy: OpenApiQueryNamingStrategy) {
+    this._queryNaming = strategy
+  }
+
+  /**
+   * Excludes one or more fields from the connectors schema
+   *
+   * @param name - The fields to exclude in dot notation
+   *               e.g. `MyType.myField`, `MyType.*.someNestedField`, `{User,Account}.email`
+   */
+  public exclude(...name: string[]) {
+    this._schemaTransforms.exclude(...name)
+  }
+}
+
+/**
+ * Header used in connector calls.
+ */
+export class QueryNamingTransform {
+  private value: OpenApiQueryNamingStrategy
+
+  constructor(value: OpenApiQueryNamingStrategy) {
+    this.value = value
+  }
+
+  public toString(): string {
+    return `queryNaming: ${this.value}`
+  }
+}
