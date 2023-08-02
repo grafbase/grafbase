2f0f1b99e2da91d9f533d6a812ea3a60ccc231bc
Jamie Barton <jamie@notrab.dev>
2023-08-02 13:19:24 +0100

    feat(template): dgraph

---
+++ templates/graphql-dgraph/grafbase/grafbase.config.ts
@@ -0,0 +1,28 @@
+import { g, connector, config } from '@grafbase/sdk'
+
+const dgraph = connector.GraphQL({
+  url: g.env('DGRAPH_API_URL')
+
+  // Enable headers if your Dgraph Cloud API requires it
+  // headers: (headers) => {
+  //   headers.set('Authorization', { forward: 'Authorization' })
+  // }
+})
+
+g.datasource(dgraph)
+
+// Use namespaces if you connect multiple APIs to avoid conflicts
+// g.datasource(dgraph, { namespace: 'dgraph' })
+
+export default config({
+  schema: g,
+  cache: {
+    rules: [
+      {
+        types: ['Query'],
+        maxAge: 60,
+        staleWhileRevalidate: 60
+      }
+    ]
+  }
+})
