db0b8a3eea4d10c778aea53b1d3b140311f0e067
Yoav Lavi <yoav@yoavlavi.com>
2023-08-03 16:47:33 +0300

    fix tests

--- packages/grafbase-sdk/jest.config.js
+++ packages/grafbase-sdk/jest.config.js
@@ -1,5 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node'
+  testEnvironment: 'node',
+  setupFiles: ['<rootDir>/tests/setup.js']
 }
