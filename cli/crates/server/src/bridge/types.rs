e44a5dacd52b96cb84bd451a1741a75e21615e34
Jakub Wieczorek <jakub@grafbase.com>
2023-08-04 16:52:55 +0200

    feat(logs): GB-4411: Nest all request-scoped log events under the operation completion entry (#582)
    # Description
    
    This adds more clarity to the log stream in the CLI.
    
    
    ![image](https://github.com/grafbase/grafbase/assets/14150873/f98b1883-6684-450e-8bb0-9eda2abb78d4)
    
    # Type of change
    
    - [ ] 💔 Breaking
    - [X] 🚀 Feature
    - [ ] 🐛 Fix
    - [ ] 🛠️ Tooling
    - [ ] 🔨 Refactoring
    - [ ] 🧪 Test
    - [ ] 📦 Dependency
    - [ ] 📖 Requires documentation update

--- cli/crates/server/src/bridge/types.rs
+++ cli/crates/server/src/bridge/types.rs
@@ -70,13 +70,33 @@ pub struct RecordDocument {
 
 #[derive(Deserialize, Debug)]
 pub struct UdfInvocation {
+    pub request_id: String,
     pub name: String,
     pub payload: serde_json::Value,
     pub udf_kind: UdfKind,
 }
 
+#[serde_with::serde_as]
+#[derive(Deserialize, Debug)]
+pub enum LogEventType {
+    OperationStarted {
+        name: Option<String>,
+    },
+    OperationCompleted {
+        name: Option<String>,
+        #[serde_as(as = "serde_with::DurationMilliSeconds<u64>")]
+        duration: std::time::Duration,
+        r#type: common::types::OperationType,
+    },
+    BadRequest {
+        name: Option<String>,
+        #[serde_as(as = "serde_with::DurationMilliSeconds<u64>")]
+        duration: std::time::Duration,
+    },
+}
+
 #[derive(Deserialize, Debug)]
 pub struct LogEvent {
     pub request_id: String,
-    pub r#type: crate::types::LogEventType,
+    pub r#type: LogEventType,
 }
