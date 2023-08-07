ee4042ebe52b8b0a2a2c0e2419887342b9694016
Jamie Barton <jamie@notrab.dev>
2023-08-07 13:03:47 +0100

    feat(examples): add fauna example

---
+++ examples/resolvers-fauna/grafbase/resolvers/products/create.ts
@@ -0,0 +1,25 @@
+import { Client, fql, FaunaError } from 'fauna'
+
+const client = new Client()
+
+export default async function ProductsCreate(_, { input }) {
+  try {
+    const documentQuery = fql`
+    products.create(${input}) {
+      id,
+      name,
+      price
+    }
+  `
+
+    const { data } = await client.query(documentQuery)
+
+    return data
+  } catch (error) {
+    if (error instanceof FaunaError) {
+      console.log(error)
+    }
+
+    return null
+  }
+}
