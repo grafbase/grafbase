b807d260cf6daf796736c402451095a5a07c5028
Jamie Barton <jamie@notrab.dev>
2023-08-22 11:47:40 +0100

    feat(examples): couchbase + resolver example

---
+++ examples/resolvers-couchbase/grafbase/resolvers/products/create.ts
@@ -0,0 +1,37 @@
+import { nanoid } from 'nanoid'
+
+/* eslint-disable turbo/no-undeclared-env-vars */
+const username = process.env.COUCHBASE_USERNAME
+const password = process.env.COUCHBASE_PASSWORD
+const url = process.env.COUCHBASE_URL
+
+export default async function ProductCreateResolver(_, { input }) {
+  const id = nanoid()
+
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Basic ' + btoa(`${username}:${password}`)
+      },
+      body: JSON.stringify({
+        statement: `INSERT INTO store (KEY, VALUE)
+          VALUES ("${id}", ${JSON.stringify({ id, ...input })});
+        `
+      })
+    })
+
+    if (!response.ok) {
+      throw new Error(await response.text())
+    }
+
+    return {
+      id,
+      ...input
+    }
+  } catch (err) {
+    console.log(err)
+    return null
+  }
+}
