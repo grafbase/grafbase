e41215dcdb56bd65a912016e743b54de4db57dd4
Yoav Lavi <yoav@yoavlavi.com>
2023-09-06 16:00:04 +0300

    add oss tooling

---
+++ engine/flake.nix
@@ -0,0 +1,71 @@
+{
+  # Nix: https://nixos.org/download.html
+  # How to activate flakes: https://nixos.wiki/wiki/Flakes
+  # For seamless integration, consider using:
+  # - direnv: https://github.com/direnv/direnv
+  # - nix-direnv: https://github.com/nix-community/nix-direnv
+  #
+  # # .envrc
+  # use flake
+  # dotenv .env
+  #
+  description = "Grafbase engine development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }: let
+    inherit (nixpkgs.lib) optional;
+    systems = flake-utils.lib.system;
+  in
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default =
+        pkgs.mkShell
+        {
+          nativeBuildInputs = with pkgs;
+            [
+              # Rust
+              cargo-make
+              cmake
+              openssl
+              pkg-config
+              rustup
+
+              # for zstd
+              libiconv
+
+              # Used to generate CLI assets
+              esbuild
+              nodePackages.pnpm
+
+              # Formatting
+              nodePackages.prettier
+
+              # Local DynamoDB handling
+              dynein
+            ]
+            ++ optional (system == systems.aarch64-darwin) [
+              darwin.apple_sdk.frameworks.Security
+              darwin.apple_sdk.frameworks.CoreFoundation
+              darwin.apple_sdk.frameworks.CoreServices
+            ];
+
+          postShellHook = ''
+            project_root="$(git rev-parse --show-toplevel 2>/dev/null || jj workspace root 2>/dev/null)"
+            export CARGO_INSTALL_ROOT="$project_root/engine/.cargo"
+            export PATH="$CARGO_INSTALL_ROOT/bin:$PATH"
+          '';
+        };
+    });
+}
