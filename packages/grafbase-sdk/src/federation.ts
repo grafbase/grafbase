51aabce909612168b4b6424f8fae8eab89d38547
Graeme Coupar <graeme@turbofish.tech>
2023-10-12 11:51:43 +0100

    feat: add federation key support to sdk

--- packages/grafbase-sdk/src/federation.ts
+++ packages/grafbase-sdk/src/federation.ts
@@ -13,3 +13,27 @@ export class Federation {
     return `\nextend schema @federation(version: "${this.version}")\n`
   }
 }
+
+export interface FederationKeyParameters {
+  resolvable?: boolean
+}
+
+const DefaultFederationParameters: FederationKeyParameters = {
+  resolvable: true
+}
+
+export class FederationKey {
+  private fields: string
+  private parameters: FederationKeyParameters
+
+  constructor(fields: string, parameters?: FederationKeyParameters) {
+    parameters = parameters || {}
+
+    this.fields = fields
+    this.parameters = { ...DefaultFederationParameters, ...parameters }
+  }
+
+  public toString(): string {
+    return `@key(fields: "${this.fields}" resolvable: ${this.parameters.resolvable})`
+  }
+}
