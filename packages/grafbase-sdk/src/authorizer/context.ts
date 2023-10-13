fdc7af07df5d2664a5630e56f9182b1d3c1bca0f
Tom Houlé <tom@tomhoule.com>
2023-10-13 07:47:50 +0200

    sdk: export type definition for authorizer arguments and return type
    It looks like this:
    
    ```typescript
    // grafbase/authorizers/myJwt.ts
    
    import { AuthorizerContext, VerifiedIdentity } from '@grafbase/sdk'
    
    export default async ({ request }: AuthorizerContext): VerifiedIdentity? {
      // ...
    }
    ```
    
    Implements the [relevant section of the RFC](https://www.notion.so/grafbase/Typed-Resolvers-b4a0b47f4b5b4d2b9815e0aa82c6a8a8?pvs=4#51dd1771bc334f3094f9ce915c6a83d3).
    
    Closes GB-5113

---
+++ packages/grafbase-sdk/src/authorizer/context.ts
@@ -0,0 +1,19 @@
+/**
+ * The type of the `context` argument in a [Custom Authorizer](https://grafbase.com/docs/auth/providers#custom-authorizer). 
+ *
+ * @example
+ * 
+ * // grafbase/authorizers/myJwt.ts
+ *
+ * import { AuthorizerContext, VerifiedIdentity } from '@grafbase/sdk'
+ *
+ * export default async ({ request }: AuthorizerContext): VerifiedIdentity? {
+ *   // ...
+ * }
+ */
+export type AuthorizerContext = {
+  /** The incoming HTTP request. */
+  request: {
+    headers: Record<string, string>
+  }
+}
---
+++ packages/grafbase-sdk/src/authorizer/verifiedIdentity.ts
@@ -0,0 +1,23 @@
+/**
+ * The data returned by a [Custom Authorizer](https://grafbase.com/docs/auth/providers#custom-authorizer) when it can verify the identity of the incoming request.
+ *
+ * @example
+ * 
+ * // grafbase/authorizers/myJwt.ts
+ *
+ * import { AuthorizerContext, VerifiedIdentity } from '@grafbase/sdk'
+ *
+ * export default async ({ request }: AuthorizerContext): VerifiedIdentity? {
+ *   // ...
+ * }
+ */
+export type VerifiedIdentity = {
+  identity: {
+    /** The identity subject (= owner). */
+    sub?: string 
+    /** Groups the authentified request belongs to. */
+    groups?: string[]
+    /** Extra, custom token claims. */
+    [tokenClaim: string]: any
+	} 
+}
