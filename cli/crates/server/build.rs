c23a8baf9baca5da53d8ab8a301f0e824c75bde1
Tom Houlé <13155277+tomhoule@users.noreply.github.com>
2023-10-17 17:09:48 +0200

    feat(cli): include Pathfinder here instead of through CDN assets (#819)
    # Description
    
    The CLI includes [Pathfinder](https://github.com/grafbase/pathfinder) as
    part of the `grafbase dev` workflow. A bundled build of Pathfinder is
    included in the assets file (`assets.tar.gz`) that is built locally when
    we iterate in this repo, and downloaded from a canonical source for CLI
    releases. The assets are included in the compiled CLI binaries, then
    decompressed to `~/.grafbase` when we start a server.
    
    Before this commit, a bundled build of Pathfinder is included in the
    assets archive that is built and downloaded separately from this repo.
    
    What this commit changes:
    
    - the Pathfinder wrapper app (the `cli-app` package) moves from the
    website repo to this repo, under `packages/`.
    - the Pathfinder build included in the CLI binary is built from source
    in this repo. **This makes updating Pathfinder much easier: we just bump
    the version in packages/cli-app/packages.json**.
    
    This commit does not change how the assets archive that is included in
    the CLI is handled. It only adds the local build of pathfinder to that
    archive when the `server` crate is built, via a cargo build script.
    
    The build script expects to find a built version of cli-app, either in
    the default location or at a path specified via
    `GRAFBASE_CLI_PATHFINDER_BUNDLE_PATH`. It will not try to build it
    itself — that can be done, but that felt like too much magic. On
    failure, it points to a relevant section in the README that explains why
    the error happens and how to build cli-app to remediate.
    
    This approach lets us progressively move the contents and construction
    logic for `assets.tar.gz` from other repos to this repo, without
    changing how the assets are consumed.
    
    Upsides:
    
    - Incremental migration away from existing assets bundling
    - Avoids introducing new include_bytes!() with their own unpacking
    logic. Keeps a single entry point for these files.
    - Easy to iterate on locally (just iterate on and build cli-app)
    - Easy to upgrade pathfinder (upgrade through cli-app package.json and
    package lock).
    
    Downsides:
    
    - The build scripts unpacks `assets.tar.gz`, adds pathfinder, then
    rebuilds the archive. We don't have the final built assets in an
    immutable location anymore.
    - Requires a JS project build to build the CLI
      - In development, you only need to build once.
    - The build process is documented and the error carefully points you to
    how to build.
    - If we want to move away from downloaded, pre-bundled assets that are
    built elsewhere, we will need to build the assets in this repo.
    - Does more work at build time, potentially making builds slower.
    - The build script will only re-run if the bundle to include changed, it
    won't make iteration slower.
    
    closes GB-4984
    
    # Type of change
    
    - [ ] 💔 Breaking
    - [ ] 🚀 Feature
    - [ ] 🐛 Fix
    - [ ] 🛠️ Tooling
    - [ ] 🔨 Refactoring
    - [ ] 🧪 Test
    - [ ] 📦 Dependency
    - [ ] 📖 Requires documentation update

---
+++ cli/crates/server/build.rs
@@ -0,0 +1,103 @@
+#![allow(clippy::panic)] // this is a build script, an explicit panic is more readable than Result
+
+use std::{env, fs, io, path::Path, time};
+
+/// Env var name
+const GRAFBASE_CLI_PATHFINDER_BUNDLE_PATH: &str = "GRAFBASE_CLI_PATHFINDER_BUNDLE_PATH";
+const ASSETS_GZIP_PATH: &str = "./assets/assets.tar.gz";
+
+/// The path to the contents of the dist/ folder of a successful build of cli-app.
+fn find_pathfinder_bundle_location() -> String {
+    eprintln!("Making sure the pathfinder app is built...");
+    if let Ok(location) = env::var(GRAFBASE_CLI_PATHFINDER_BUNDLE_PATH) {
+        assert!(
+            fs::metadata(&location).is_ok(),
+            "The location specified in {GRAFBASE_CLI_PATHFINDER_BUNDLE_PATH} ({location} does ont exist."
+        );
+        return location;
+    }
+
+    let default_path = "../../../packages/cli-app/dist";
+
+    eprintln!("Using the default location for the pathfinder app...");
+    if fs::metadata(default_path).is_ok() {
+        return default_path.to_owned();
+    }
+
+    panic!(
+        r#"
+Could not find a bundled pathfinder to include in cli binary.
+
+You must either provide the GRAFBASE_CLI_PATHFINDER_BUNDLE_PATH environment variable or
+build the Pathfinder app at the default location.
+
+Please see the instructions in cli/crates/server/README.md.
+    "#
+    );
+}
+
+fn decompress_assets() -> io::Result<tempfile::TempDir> {
+    use flate2::bufread::GzDecoder;
+    let dir = tempfile::tempdir()?;
+    eprintln!("Decompressing the assets at `{ASSETS_GZIP_PATH}` from `server` crate build script.");
+    let file_reader = io::BufReader::new(fs::File::open(ASSETS_GZIP_PATH)?);
+    let assets_reader = GzDecoder::new(file_reader);
+    tar::Archive::new(assets_reader).unpack(dir.path())?;
+    Ok(dir)
+}
+
+fn recompress_assets(assets_path: &Path) -> io::Result<()> {
+    use flate2::{write::GzEncoder, Compression};
+
+    eprintln!("Reconstructing assets.tar.gz");
+    let out_file_path = Path::new(&env::var("OUT_DIR").unwrap()).join("assets.tar.gz");
+    eprintln!("   Creating {out_file_path:?}");
+    let out_file = fs::File::create(out_file_path)?;
+    let out = GzEncoder::new(out_file, Compression::default());
+    let mut tar_builder = tar::Builder::new(out);
+    tar_builder.mode(tar::HeaderMode::Deterministic);
+    eprintln!("   Adding {assets_path:?} to archive");
+    tar_builder.append_dir_all(".", assets_path).unwrap();
+    tar_builder.finish()?;
+
+    Ok(())
+}
+
+fn main() -> io::Result<()> {
+    let start = time::Instant::now();
+
+    let tmp_assets = decompress_assets()?;
+    eprintln!(
+        "⏱️ Timing after decompress_assets(): {:?}",
+        time::Instant::now().duration_since(start)
+    );
+    let bundle_location = find_pathfinder_bundle_location();
+    eprintln!(
+        "⏱️ Timing after find_pathfinder_bundle_location(): {:?}",
+        time::Instant::now().duration_since(start)
+    );
+
+    eprintln!("Copying bundled pathfinder to the assets dir...");
+    let target_path = tmp_assets.path().join("static/assets");
+    fs::create_dir_all(&target_path)?;
+    for file in fs::read_dir(Path::new(&bundle_location).join("assets"))? {
+        let file_path = file?.path();
+        let dest_path = target_path.join(file_path.file_name().unwrap());
+        eprintln!("    {file_path:?} -> {dest_path:?}");
+        fs::copy(file_path, &dest_path)?;
+    }
+    eprintln!("⏱️ Timing after copy: {:?}", time::Instant::now().duration_since(start));
+
+    recompress_assets(tmp_assets.path())?;
+    eprintln!(
+        "⏱️ Timing after recompress: {:?}",
+        time::Instant::now().duration_since(start)
+    );
+
+    // Tell Cargo to rerun this script only if the assets or the pathfinder bundle changed.
+    println!("cargo:rerun-if-changed={bundle_location}");
+    println!("cargo:rerun-if-env-changed={GRAFBASE_CLI_PATHFINDER_BUNDLE_PATH}");
+    println!("cargo:rerun-if-changed={ASSETS_GZIP_PATH}");
+
+    Ok(())
+}
