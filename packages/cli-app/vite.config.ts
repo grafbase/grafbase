a3b1679803950a971df3b3ac95ac4f22ee0419af
Jonathan Brennan <jonathanawesome@users.noreply.github.com>
2023-11-01 11:01:35 -0500

    use "/static" for base path when running "serve"

--- packages/cli-app/vite.config.ts
+++ packages/cli-app/vite.config.ts
@@ -3,13 +3,14 @@ import { defineConfig } from 'vite'
 
 export default defineConfig(({ command }) => {
   return {
+    base: command === 'serve' ? '/' : '/static',
     build: {
       rollupOptions: {
         output: {
           // this prevents vite from hashing assets (produces index.js rather than index-XXXXXXXX.js)
-          entryFileNames: 'static/assets/[name].js',
-          chunkFileNames: 'static/assets/[name].js',
-          assetFileNames: 'static/assets/[name].[ext]'
+          entryFileNames: `assets/[name].js`,
+          chunkFileNames: `assets/[name].js`,
+          assetFileNames: `assets/[name].[ext]`
         }
       }
     },
