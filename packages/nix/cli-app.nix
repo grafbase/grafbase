cd6ca5ba5f5cc7a4fe0b292fc4d3777b25b8f462
Yoav Lavi <yoav@yoavlavi.com>
2023-11-09 16:52:51 +0200

    fix cli-app

--- packages/nix/cli-app.nix
+++ packages/nix/cli-app.nix
@@ -7,15 +7,15 @@
     /crates
     /engine
     /cli
-    /nix
+    /packages/nix
     *.nix
   '';
-  src = pkgs.nix-gitignore.gitignoreSource extraIgnores ../../.;
+  src = pkgs.nix-gitignore.gitignoreSourcePure [extraIgnores ".gitignore"] ../../packages;
 in
   mkPnpmPackage {
     inherit src;
     name = "cli-app";
     installInPlace = true;
-    distDir = "packages/cli-app/dist";
+    distDir = "../packages/cli-app/dist";
     script = "build-cli-app";
   }
