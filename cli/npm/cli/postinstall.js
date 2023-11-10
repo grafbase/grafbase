a5e64976e29482beb0963b17dacce9b1b1d94563
Fredrik Björk <fredrik.bjork@gmail.com>
2023-11-10 11:37:59 +0100

    format

--- cli/npm/cli/postinstall.js
+++ cli/npm/cli/postinstall.js
@@ -3,9 +3,9 @@
 // which is based on
 // https://github.com/parcel-bundler/parcel-css/blob/master/cli/postinstall.js
 
-const { existsSync, linkSync, copyFileSync } = require('fs');
-const { dirname, join } = require('path');
-const { getEnvironmentData } = require('./get-environment-data');
+const { existsSync, linkSync, copyFileSync } = require('fs')
+const { dirname, join } = require('path')
+const { getEnvironmentData } = require('./get-environment-data')
 
 const SUPPORTED_TARGET_TRIPLES = [
   'x86_64-apple-darwin',
@@ -13,38 +13,38 @@ const SUPPORTED_TARGET_TRIPLES = [
   'x86_64-unknown-linux-musl',
   'aarch64-unknown-linux-musl',
   'x86_64-pc-windows-msvc',
-];
+]
 
 const linkBinary = (binary, binaryPath) => {
   try {
-    linkSync(binaryPath, join(__dirname, binary));
+    linkSync(binaryPath, join(__dirname, binary))
   } catch {
-    copyFileSync(binaryPath, join(__dirname, binary));
+    copyFileSync(binaryPath, join(__dirname, binary))
   }
-};
+}
 
 const postinstall = () => {
-  const { targetTriple, binary } = getEnvironmentData('grafbase');
+  const { targetTriple, binary } = getEnvironmentData('grafbase')
 
   if (!SUPPORTED_TARGET_TRIPLES.includes(targetTriple)) {
-    console.error(`'${targetTriple}' is currently unsupported`);
-    return;
+    console.error(`'${targetTriple}' is currently unsupported`)
+    return
   }
 
-  const packagePath = dirname(require.resolve(`@grafbase/cli-${targetTriple}/package.json`));
-  const binaryPath = join(packagePath, binary);
+  const packagePath = dirname(require.resolve(`@grafbase/cli-${targetTriple}/package.json`))
+  const binaryPath = join(packagePath, binary)
 
   if (existsSync(binaryPath)) {
     try {
-      linkBinary(binary, binaryPath);
+      linkBinary(binary, binaryPath)
     } catch {
-      console.error('Could not link or copy "grafbase" binary');
-      return;
+      console.error('Could not link or copy "grafbase" binary')
+      return
     }
   } else {
-    console.error('Could not find "grafbase" binary');
-    return;
+    console.error('Could not find "grafbase" binary')
+    return
   }
-};
+}
 
-postinstall();
+postinstall()
