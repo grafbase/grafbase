9eeeaf7916dd245449da1798480ded9f254a4298
Yoav Lavi <yoav@yoavlavi.com>
2023-11-16 15:54:19 +0200

    test name

--- packages/grafbase-sdk/tests/unit/config.test.ts
+++ packages/grafbase-sdk/tests/unit/config.test.ts
@@ -2,8 +2,8 @@ import { config, graph } from '../../src/index'
 import { describe, expect, it } from '@jest/globals'
 import { renderGraphQL } from '../utils'
 
-describe('Federation config', () => {
-  it('renders a graph directive that extends the schema', () => {
+describe('Config', () => {
+  it('renders a configuration that uses extra configuration settings', () => {
     const cfg = config({
       graph: graph.Single(),
       experimental: {
