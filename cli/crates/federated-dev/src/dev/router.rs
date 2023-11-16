7c22dd7fbfcc42284a4afb4b13a866d5f8b325b8
Graeme Coupar <graeme@turbofish.tech>
2023-11-16 11:57:09 +0000

    feat: hook federation-dev up to engine-v2

--- cli/crates/federated-dev/src/dev/router.rs
+++ cli/crates/federated-dev/src/dev/router.rs
@@ -1,28 +1,76 @@
-use super::bus::GraphReceiver;
+use std::sync::Arc;
+
+use super::bus::{GraphReceiver, RequestReceiver, ResponseSender};
+use engine::ServerError;
+use engine_v2::Engine;
+use futures_concurrency::stream::Merge;
+use futures_util::{stream::BoxStream, StreamExt};
+use graphql_composition::FederatedGraph;
+use tokio_stream::wrappers::ReceiverStream;
 
 pub(crate) struct Router {
-    bus: GraphReceiver,
+    graph_bus: GraphReceiver,
+    request_bus: RequestReceiver,
+    engine: Option<Arc<Engine>>,
 }
 
-/// TODO: Benjamin and Graeme!
-///
-/// Add the router here, route the queries from the Axum to it.
-/// When the handler receives a new graph, restart the router.
-///
-/// P.s. there might be a need for a m-m-mutex...
 impl Router {
-    pub fn new(bus: GraphReceiver) -> Self {
-        Self { bus }
+    pub fn new(graph_bus: GraphReceiver, request_bus: RequestReceiver) -> Self {
+        Self {
+            graph_bus,
+            request_bus,
+            engine: None,
+        }
     }
 
     pub async fn handler(mut self) -> Result<(), crate::Error> {
-        while let Some(graph) = self.bus.recv().await {
-            // remvove this when adding the actual router
-            let rendered = graphql_composition::render_sdl(&graph).expect("must render");
-            println!("******** GRAPH ********");
-            print!("{rendered}");
+        let streams: [RouterStream; 2] = [
+            Box::pin(ReceiverStream::new(self.graph_bus).map(RouterMessage::Graph)),
+            Box::pin(ReceiverStream::new(self.request_bus).map(RouterMessage::request)),
+        ];
+
+        let mut stream = streams.merge();
+
+        while let Some(message) = stream.next().await {
+            match (message, self.engine.as_ref()) {
+                (RouterMessage::Graph(graph), _) => {
+                    self.engine = Some(new_engine(graph));
+                }
+                (RouterMessage::Request(request, response_sender), Some(engine)) => {
+                    tokio::spawn(run_request(request, response_sender, Arc::clone(engine)));
+                }
+                (RouterMessage::Request(_, response_sender), None) => {
+                    response_sender
+                        .send(Err(ServerError::new(
+                            "there are no subgraphs registered currently",
+                            None,
+                        )))
+                        .ok();
+                }
+            }
         }
 
         Ok(())
     }
 }
+
+fn new_engine(graph: FederatedGraph) -> Arc<Engine> {
+    Arc::new(Engine::new(graph.into()))
+}
+
+async fn run_request(request: engine::Request, response_sender: ResponseSender, engine: Arc<Engine>) {
+    response_sender.send(engine.execute_request(request).await).ok();
+}
+
+enum RouterMessage {
+    Graph(FederatedGraph),
+    Request(engine::Request, ResponseSender),
+}
+
+impl RouterMessage {
+    fn request((request, sender): (engine::Request, ResponseSender)) -> Self {
+        RouterMessage::Request(request, sender)
+    }
+}
+
+type RouterStream = BoxStream<'static, RouterMessage>;
--- engine/crates/engine-v2/src/engine.rs
+++ engine/crates/engine-v2/src/engine.rs
@@ -1,4 +1,4 @@
-use engine::ServerResult;
+use engine::{ServerError, ServerResult};
 use engine_parser::types::OperationDefinition;
 use schema::Schema;
 
@@ -11,6 +11,26 @@ pub struct Engine {
 impl Engine {
     pub fn new(schema: Schema) -> Self {
         Self { schema }
+    }
+
+    pub async fn execute_request(&self, request: engine::Request) -> ServerResult<serde_json::Value> {
+        let document = engine_parser::parse_query(request.query)?;
+
+        let mut operations = document.operations.iter();
+
+        let operation = match request.operation_name {
+            None => operations
+                .next()
+                .ok_or_else(|| ServerError::new("document contains no operations", None))?,
+            Some(expected_name) => operations
+                .find(|(name, _)| name.is_some() && *name.unwrap() == expected_name)
+                .ok_or_else(|| ServerError::new(format!("could not find an operation named {expected_name}"), None))?,
+        }
+        .1
+        .clone()
+        .node;
+
+        self.execute(operation).await
     }
 
     pub async fn execute(&self, operation_definition: OperationDefinition) -> ServerResult<serde_json::Value> {
