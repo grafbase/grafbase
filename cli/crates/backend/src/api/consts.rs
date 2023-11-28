6aeebc1c4f7d56c14dcc36b538c4619ef4517f2e
Yoav Lavi <yoav@yoavlavi.com>
2023-11-28 13:57:32 +0200

    wip

--- cli/crates/backend/src/api/consts.rs
+++ cli/crates/backend/src/api/consts.rs
@@ -1,7 +1,7 @@
 pub const CREDENTIALS_FILE: &str = "credentials.json";
 pub const PROJECT_METADATA_FILE: &str = "project.json";
-pub const AUTH_URL: &str = "https://grafbase.dev/auth/cli";
-pub const API_URL: &str = "https://api.grafbase.dev/graphql";
+pub const AUTH_URL: &str = "https://grafbase.com/auth/cli";
+pub const API_URL: &str = "https://api.grafbase.com/graphql";
 pub const PACKAGE_JSON: &str = "package.json";
 pub const TAR_CONTENT_TYPE: &str = "application/x-tar";
 pub const GRAFBASE_ACCESS_TOKEN_ENV_VAR: &str = "GRAFBASE_ACCESS_TOKEN";
