f5e8b019c768aed10cafa688a78f4b7e3fff0018
Tom Houlé <tom@tomhoule.com>
2023-12-06 17:46:11 +0100

    cli: rename --subgraph to --name, document reading schema from stdin

--- cli/crates/cli/src/cli_input/check.rs
+++ cli/crates/cli/src/cli_input/check.rs
@@ -6,9 +6,11 @@ pub struct CheckCommand {
     pub project_ref: ProjectRef,
     /// The name of the subgraph to check. This argument is always required in a federated graph
     /// context, and it should not be used in a single graph context.
-    #[arg(long)]
-    pub subgraph: Option<String>,
-    /// The path to the GraphQL schema to check.
+    #[arg(long("name"))]
+    pub(crate) subgraph_name: Option<String>,
+
+    /// The path to the GraphQL schema to check. If this is not provided, the schema will be read
+    /// from stdin.
     #[arg(long)]
     pub schema: Option<String>,
 }
