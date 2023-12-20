7c8b42276508c38ae53c81ea23044049a6aad796
Graeme Coupar <graeme@grafbase.com>
2023-12-20 14:43:14 +0000

    chore: remove sqlx dependency (#1161)
    I've been (lightly) obsessing over CI build times the past few days -
    and noticed we were still building sqlite/sqlx etc. in the CLI even
    though the database feature has been removed.
    
    This is probably because `proxy.rs` had a `#[allow(unused)]` that was
    hiding quite a few unused warnings. This removes that, fixes the
    warnings, removes sqlite.

--- cli/crates/server/src/errors.rs
+++ cli/crates/server/src/errors.rs
@@ -6,7 +6,6 @@ use common::types::UdfKind;
 use hyper::Error as HyperError;
 use notify::Error as NotifyError;
 use serde_json::json;
-use sqlx::Error as SqlxError;
 use std::io::Error as IoError;
 use std::path::PathBuf;
 use thiserror::Error;
@@ -48,22 +47,6 @@ pub enum ServerError {
     /// returned if the version of the existing worker files cannot be read
     #[error("could not read the previously extracted embedded file versions")]
     ReadVersion,
-
-    /// returned if a connection to the sqlite database could not be made
-    #[error("could not connect to the sqlite database: {0}")]
-    ConnectToDatabase(SqlxError),
-
-    /// returned if an sqlite database file cannot be created
-    #[error("could not create an sqlite database file: {0}")]
-    CreateDatabase(SqlxError),
-
-    /// returned if an sqlite query returns an error
-    #[error("could not query the sqlite database: {0}")]
-    QueryDatabase(SqlxError),
-
-    /// returned if sqlx returns an unknown error
-    #[error("encountered an unknown sqlite error: {0}")]
-    UnknownSqliteError(SqlxError),
 
     /// returned if the sqlite bridge cannot be started
     #[error("the bridge api encountered an error: {0}")]
@@ -254,32 +237,6 @@ pub enum UdfBuildError {
     /// returned if a spawned task panics
     #[error(transparent)]
     SpawnedTaskPanic(#[from] JoinError),
-}
-
-impl From<SqlxError> for ServerError {
-    fn from(error: SqlxError) -> Self {
-        match error {
-            SqlxError::RowNotFound
-            | SqlxError::TypeNotFound { .. }
-            | SqlxError::ColumnNotFound(_)
-            | SqlxError::ColumnDecode { .. }
-            | SqlxError::ColumnIndexOutOfBounds { .. }
-            | SqlxError::Io(_)
-            | SqlxError::Decode(_)
-            | SqlxError::Database(_) => Self::QueryDatabase(error),
-
-            SqlxError::Configuration(_)
-            | SqlxError::Tls(_)
-            | SqlxError::PoolTimedOut
-            | SqlxError::Protocol(_)
-            | SqlxError::PoolClosed
-            | SqlxError::WorkerCrashed => Self::ConnectToDatabase(error),
-
-            SqlxError::Migrate(_) => Self::CreateDatabase(error),
-
-            _ => Self::UnknownSqliteError(error),
-        }
-    }
 }
 
 impl IntoResponse for ServerError {
