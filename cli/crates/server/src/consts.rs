38473447fd0f27c6845275ec3e4339fa6219c156
Benjamin Rabier <benjamin+git@rabier.dev>
2024-01-10 10:12:43 +0100

    chore: Add federation start test in TS

--- cli/crates/server/src/consts.rs
+++ cli/crates/server/src/consts.rs
@@ -1,5 +1,5 @@
 pub const ASSET_VERSION_FILE: &str = "version.txt";
-pub const CONFIG_PARSER_SCRIPT: &str = "parse-config.ts";
+pub const CONFIG_PARSER_SCRIPT: &str = "parse-config.mts";
 pub const DOT_ENV_FILE_NAME: &str = ".env";
 pub const GIT_IGNORE_CONTENTS: &str = "*\n";
 pub const GIT_IGNORE_FILE: &str = ".gitignore";
