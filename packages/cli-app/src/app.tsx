e1f306fce76c67f21ac4065c9537aa088d8ae9a3
Yoav Lavi <yoav@yoavlavi.com>
2024-01-29 20:28:25 +0200

    pathfinder-0.5.0 (#1298)
    # Description
    
    ## Features
    
    - Updates Pathfinder
    
    # Type of change
    
    - [ ] 💔 Breaking
    - [x] 🚀 Feature
    - [ ] 🐛 Fix
    - [ ] 🛠️ Tooling
    - [ ] 🔨 Refactoring
    - [ ] 🧪 Test
    - [ ] 📦 Dependency
    - [ ] 📖 Requires documentation update

--- packages/cli-app/src/app.tsx
+++ packages/cli-app/src/app.tsx
@@ -11,6 +11,18 @@ export const App = () => {
   return (
     <div className="wrap">
       <Pathfinder
+        watchHeaders={[
+          {
+            headerName: 'x-grafbase-cache',
+            responseMap: {
+              HIT: { value: 'CACHE: HIT', color: 'green' },
+              MISS: { value: 'CACHE: MISS', color: 'red' },
+              UPDATING: { value: 'CACHE: UPDATING', color: 'blue' },
+              STALE: { value: 'CACHE: STALE', color: 'purple' },
+              BYPASS: { value: 'CACHE: BYPASS', color: 'yellow' }
+            }
+          }
+        ]}
         fetcherOptions={{
           endpoint,
           headers: [
