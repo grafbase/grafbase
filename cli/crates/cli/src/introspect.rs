c6997885e2a46f32f6a4ba947fa905572846dadd
Tom Houlé <13155277+tomhoule@users.noreply.github.com>
2024-01-31 19:13:11 +0100

    cli: add GraphQL syntax highlighting in introspect (#1303)
    - Implemented using [syntect](https://crates.io/crates/syntect).
    - Only for `grafbase introspect` at first.
    - Comes with a `--no-color` flag to disable.
    - Takes into account the [`NO_COLOR` standard](https://no-color.org/)
    - Does not add ansi colors when used in pipes
    - Adds nearly exactly 1MB to the release binaries
    - Uses the solarized dark theme because that's the only builtin dark
    theme that works well on my light background terminal. We could look
    into [bat's
    themes](https://github.com/sharkdp/bat/tree/master/assets/themes) or
    make our own for alternative options
    
    closes GB-5888

--- cli/crates/cli/src/introspect.rs
+++ cli/crates/cli/src/introspect.rs
@@ -5,9 +5,9 @@ pub(crate) fn introspect(command: &IntrospectCommand) -> Result<(), CliError> {
     match (command.url(), command.dev) {
         (Some(url), _) => {
             let headers = command.headers().collect::<Vec<_>>();
-            introspect_remote(url, &headers)
+            introspect_remote(url, &headers, command.no_color)
         }
-        (None, true) => introspect_local(),
+        (None, true) => introspect_local(command.no_color),
         (None, false) => {
             eprintln!("Error: Either the --url or the --dev argument must be provided.");
             std::process::exit(1);
@@ -15,10 +15,10 @@ pub(crate) fn introspect(command: &IntrospectCommand) -> Result<(), CliError> {
     }
 }
 
-fn introspect_local() -> Result<(), CliError> {
+fn introspect_local(no_color: bool) -> Result<(), CliError> {
     match server::introspect_local().map_err(CliError::ServerError)? {
         server::IntrospectLocalOutput::Sdl(schema) => {
-            println!("{schema}");
+            print_introspected_schema(&schema, no_color);
         }
         server::IntrospectLocalOutput::EmptyFederated => {
             report::federated_schema_local_introspection_not_implemented();
@@ -28,11 +28,55 @@ fn introspect_local() -> Result<(), CliError> {
     Ok(())
 }
 
-fn introspect_remote(url: &str, headers: &[(&str, &str)]) -> Result<(), CliError> {
+fn introspect_remote(url: &str, headers: &[(&str, &str)], no_color: bool) -> Result<(), CliError> {
     let operation = graphql_introspection::introspect(url, headers);
 
     match Runtime::new().unwrap().block_on(operation) {
-        Ok(result) => Ok(println!("{result}")),
+        Ok(result) => {
+            print_introspected_schema(&result, no_color);
+            Ok(())
+        }
         Err(e) => Err(CliError::Introspection(e)),
     }
+}
+
+fn print_introspected_schema(sdl: &str, no_color: bool) {
+    use syntect::util::{as_24_bit_terminal_escaped, LinesWithEndings};
+
+    // No highlighting when stdout is not a tty (likely a pipe) or when explicitly requested.
+    if no_color || !atty::is(atty::Stream::Stdout) || no_color_env_var() {
+        println!("{sdl}");
+        return;
+    }
+
+    const GRAMMAR: &str = include_str!("introspect/graphql_grammar.yml");
+
+    let mut builder = syntect::parsing::SyntaxSetBuilder::new();
+    builder.add(
+        syntect::parsing::SyntaxDefinition::load_from_str(GRAMMAR, false, Some("graphql"))
+            .expect("Loading the bundled grammar"),
+    );
+    let syntax_set = builder.build();
+
+    let graphql = syntax_set
+        .find_syntax_by_extension("graphql")
+        .expect("graphql syntax to be bundled");
+    let theme_set = syntect::highlighting::ThemeSet::load_defaults();
+    let theme = &theme_set.themes["Solarized (dark)"];
+
+    let mut highlighter = syntect::easy::HighlightLines::new(graphql, theme);
+
+    for line in LinesWithEndings::from(sdl) {
+        let ranges = highlighter
+            .highlight_line(line, &syntax_set)
+            .expect("line to be highlightable");
+
+        let escaped = as_24_bit_terminal_escaped(&ranges[..], false);
+        print!("{}", escaped);
+    }
+}
+
+/// https://no-color.org/
+fn no_color_env_var() -> bool {
+    std::env::var("NO_COLOR").is_ok_and(|value| !value.is_empty())
 }
