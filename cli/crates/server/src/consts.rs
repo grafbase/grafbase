d6ff0af91f67e6e5e173f79cbeff131066ff4d7a
Yoav Lavi <yoav@yoavlavi.com>
2024-02-05 17:36:46 +0200

    remove additional build

--- cli/crates/server/src/consts.rs
+++ cli/crates/server/src/consts.rs
@@ -9,4 +9,4 @@ pub const SCHEMA_PARSER_DIR: &str = "parser";
 pub const TS_NODE_SCRIPT_PATH: &str = "node_modules/ts-node/dist/bin.js";
 pub const WRAPPER_WORKER_JS_PATH: &str = "custom-resolvers/wrapper.js";
 pub const MULTI_WRAPPER_WORKER_JS_PATH: &str = "custom-resolvers/bun-multi-wrapper.ts";
-pub const ENTRYPOINT_SCRIPT_FILE_NAME: &str = "entrypoint.ts";
+pub const ENTRYPOINT_SCRIPT_FILE_NAME: &str = "entrypoint.js";
