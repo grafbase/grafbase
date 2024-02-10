d57d2afc8d381e383de042febe7743cb1847cb72
Graeme Coupar <graeme@turbofish.tech>
2024-02-10 21:59:59 +0000

    chore: pull in graphql-ws-client 0.8.0-rc.1
    I wasn't especially happy with the API for graphql-ws-client when I was
    building subscriptions last month, so I've been spending a couple of weekends
    improving things.
    
    This PR pulls in the release candidate I just put out, so I can be sure it
    works for our use case.

--- cli/crates/cli/tests/utils/async_client/websockets.rs
+++ cli/crates/cli/tests/utils/async_client/websockets.rs
@@ -16,62 +16,39 @@ where
         use async_tungstenite::tungstenite::{client::IntoClientRequest, http::HeaderValue};
         use futures_util::StreamExt;
 
-        let mut client = {
-            let (mut request, payload_headers) = {
-                let (client, request) = self.reqwest_builder.build_split();
-                let request = request.unwrap();
+        let (mut request, payload_headers) = {
+            let (client, request) = self.reqwest_builder.build_split();
+            let request = request.unwrap();
 
-                // make sure we can still use self below
-                self.reqwest_builder = client.request(Method::GET, "http://example.com");
+            // make sure we can still use self below
+            self.reqwest_builder = client.request(Method::GET, "http://example.com");
 
-                let payload_headers = request
-                    .headers()
-                    .iter()
-                    .map(|(k, v)| (k.to_string(), v.to_str().unwrap_or_default().to_string()))
-                    .collect::<BTreeMap<_, _>>();
+            let payload_headers = request
+                .headers()
+                .iter()
+                .map(|(k, v)| (k.to_string(), v.to_str().unwrap_or_default().to_string()))
+                .collect::<BTreeMap<_, _>>();
 
-                let mut url = request.url().clone();
-                url.set_path("/ws");
-                url.set_scheme("ws").unwrap();
-                let request = url.into_client_request().unwrap();
+            let mut url = request.url().clone();
+            url.set_path("/ws");
+            url.set_scheme("ws").unwrap();
+            let request = url.into_client_request().unwrap();
 
-                (request, payload_headers)
-            };
-
-            request.headers_mut().insert(
-                "Sec-WebSocket-Protocol",
-                HeaderValue::from_str("graphql-transport-ws").unwrap(),
-            );
-
-            let (connection, _) = async_tungstenite::tokio::connect_async(request).await.unwrap();
-
-            let (sink, stream) = connection.split();
-
-            graphql_ws_client::AsyncWebsocketClientBuilder::<CliGraphqlClient>::new()
-                .payload(json!({ "headers": payload_headers }))
-                .build(stream, sink, TokioSpawner::current())
-                .await?
+            (request, payload_headers)
         };
 
-        Ok(client.streaming_operation(self).await?.map(move |item| {
-            // Ignore this next line, I'm just tricking rust into
-            // moving the client into this closure.
-            let _client = &client;
+        request.headers_mut().insert(
+            "Sec-WebSocket-Protocol",
+            HeaderValue::from_str("graphql-transport-ws").unwrap(),
+        );
 
-            item.unwrap()
-        }))
-    }
-}
+        let (connection, _) = async_tungstenite::tokio::connect_async(request).await.unwrap();
 
-pub struct CliGraphqlClient;
-
-impl graphql_ws_client::graphql::GraphqlClient for CliGraphqlClient {
-    type Response = serde_json::Value;
-
-    type DecodeError = serde_json::Error;
-
-    fn error_response(errors: Vec<serde_json::Value>) -> Result<Self::Response, Self::DecodeError> {
-        Ok(json!({"errors": errors}))
+        Ok(graphql_ws_client::Client::build(connection)
+            .payload(json!({ "headers": payload_headers }))?
+            .subscribe(self)
+            .await?
+            .map(|item| item.unwrap()))
     }
 }
 
@@ -79,32 +56,11 @@ impl<T> graphql_ws_client::graphql::GraphqlOperation for GqlRequestBuilder<T>
 where
     T: serde::de::DeserializeOwned,
 {
-    type GenericResponse = serde_json::Value;
-
     type Response = T;
 
     type Error = serde_json::Error;
 
-    fn decode(&self, data: Self::GenericResponse) -> Result<Self::Response, Self::Error> {
+    fn decode(&self, data: serde_json::Value) -> Result<Self::Response, Self::Error> {
         serde_json::from_value(data)
-    }
-}
-
-pub struct TokioSpawner(tokio::runtime::Handle);
-
-impl TokioSpawner {
-    pub fn new(handle: tokio::runtime::Handle) -> Self {
-        TokioSpawner(handle)
-    }
-
-    pub fn current() -> Self {
-        TokioSpawner::new(tokio::runtime::Handle::current())
-    }
-}
-
-impl futures_util::task::Spawn for TokioSpawner {
-    fn spawn_obj(&self, obj: futures_util::task::FutureObj<'static, ()>) -> Result<(), futures_util::task::SpawnError> {
-        self.0.spawn(obj);
-        Ok(())
     }
 }
--- engine/crates/runtime-local/src/fetch/websockets.rs
+++ engine/crates/runtime-local/src/fetch/websockets.rs
@@ -1,19 +1,6 @@
 //! graphql-ws-client <> engine glue code
 
 use runtime::fetch::{FetchError, GraphqlRequest};
-use serde_json::json;
-
-pub struct EngineGraphqlClient;
-
-impl graphql_ws_client::graphql::GraphqlClient for EngineGraphqlClient {
-    type Response = serde_json::Value;
-
-    type DecodeError = FetchError;
-
-    fn error_response(errors: Vec<serde_json::Value>) -> Result<Self::Response, Self::DecodeError> {
-        Ok(json!({"errors": errors}))
-    }
-}
 
 #[derive(serde::Serialize)]
 pub struct StreamingRequest {
@@ -31,32 +18,11 @@ impl From<GraphqlRequest<'_>> for StreamingRequest {
 }
 
 impl graphql_ws_client::graphql::GraphqlOperation for StreamingRequest {
-    type GenericResponse = serde_json::Value;
-
     type Response = serde_json::Value;
 
     type Error = FetchError;
 
-    fn decode(&self, data: Self::GenericResponse) -> Result<Self::Response, Self::Error> {
+    fn decode(&self, data: serde_json::Value) -> Result<Self::Response, Self::Error> {
         Ok(data)
-    }
-}
-
-pub struct TokioSpawner(tokio::runtime::Handle);
-
-impl TokioSpawner {
-    pub fn new(handle: tokio::runtime::Handle) -> Self {
-        TokioSpawner(handle)
-    }
-
-    pub fn current() -> Self {
-        TokioSpawner::new(tokio::runtime::Handle::current())
-    }
-}
-
-impl futures_util::task::Spawn for TokioSpawner {
-    fn spawn_obj(&self, obj: futures_util::task::FutureObj<'static, ()>) -> Result<(), futures_util::task::SpawnError> {
-        self.0.spawn(obj);
-        Ok(())
     }
 }
