b3358f90d3d245801046565c2995a04d5858b69c
Tom Houlé <tom@tomhoule.com>
2024-02-15 18:00:00 +0100

    cli: avoid crashing when stdout becomes unavailable in introspect
    This is the well known issue of `print!()` & co from the standard
    library panicking if stdout/stderr becomes unavailable. This commit
    fixes the issue in `grafbase introspect`.
    
    closes GB-5788

--- cli/nix/udf-wrapper.nix
+++ cli/nix/udf-wrapper.nix
@@ -4,7 +4,7 @@
   packages.udf-wrapper = pkgs.buildNpmPackage {
     src = ../udf-wrapper;
     name = "udf-wrapper";
-    npmDepsHash = "sha256-xUNkgpuwy75kP1P7pVTguVDCQUa9nvArml2PyOMuBYM=";
+    npmDepsHash = "sha256-+74Y5QTvYVpX7Yv8SLp9+lQT5qk3ZNTXE0RksdEF3HI=";
 
     nativeBuildInputs = [ pkgs.bun ];
 
