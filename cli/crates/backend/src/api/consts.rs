c56e4f3c3153871043225fe7c7b3e164021a1298
Jakob Norlin <jakob.stahl91@gmail.com>
2024-02-23 14:22:38 +0100

    Switch CLI to dev environment

--- cli/crates/backend/src/api/consts.rs
+++ cli/crates/backend/src/api/consts.rs
@@ -1,12 +1,12 @@
 use std::sync::OnceLock;
 
 pub const CREDENTIALS_FILE: &str = "credentials.json";
-pub const AUTH_URL: &str = "https://app.grafbase.com/auth/cli";
+pub const AUTH_URL: &str = "https://app.grafbase.dev/auth/cli";
 pub const PACKAGE_JSON: &str = "package.json";
 pub const TAR_CONTENT_TYPE: &str = "application/x-tar";
 pub const GRAFBASE_ACCESS_TOKEN_ENV_VAR: &str = "GRAFBASE_ACCESS_TOKEN";
 
-const API_URL: &str = "https://api.grafbase.com/graphql";
+const API_URL: &str = "https://api.grafbase.dev/graphql";
 
 pub fn api_url() -> &'static str {
     static API_URL: OnceLock<String> = OnceLock::new();
