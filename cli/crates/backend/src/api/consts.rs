2d119493c8536aae80a5ab57cc4313b0cd5118ec
Tom Houlé <tom@tomhoule.com>
2024-02-23 13:17:04 +0100

    special sign in url

--- cli/crates/backend/src/api/consts.rs
+++ cli/crates/backend/src/api/consts.rs
@@ -1,6 +1,6 @@
 pub const CREDENTIALS_FILE: &str = "credentials.json";
-pub const AUTH_URL: &str = "https://grafbase.com/auth/cli";
-pub const API_URL: &str = "https://api.grafbase.com/graphql";
+pub const AUTH_URL: &str = "https://dashboard-git-norlin-sign-in-flow.grafbase-vercel.dev/sign-in";
+pub const API_URL: &str = "https://api.grafbase.dev/graphql";
 pub const PACKAGE_JSON: &str = "package.json";
 pub const TAR_CONTENT_TYPE: &str = "application/x-tar";
 pub const GRAFBASE_ACCESS_TOKEN_ENV_VAR: &str = "GRAFBASE_ACCESS_TOKEN";
