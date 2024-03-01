8394669b357903725b1645855e463bc6e5b04e34
Jakob Norlin <jakob.stahl91@gmail.com>
2024-03-01 11:13:05 +0100

    Change auth flow to use new dashboard

--- cli/crates/backend/src/api/consts.rs
+++ cli/crates/backend/src/api/consts.rs
@@ -1,5 +1,5 @@
 pub const CREDENTIALS_FILE: &str = "credentials.json";
-pub const AUTH_URL: &str = "https://grafbase.com/auth/cli";
+pub const AUTH_URL: &str = "https://app.grafbase.com/auth/cli";
 pub const API_URL: &str = "https://api.grafbase.com/graphql";
 pub const PACKAGE_JSON: &str = "package.json";
 pub const TAR_CONTENT_TYPE: &str = "application/x-tar";
