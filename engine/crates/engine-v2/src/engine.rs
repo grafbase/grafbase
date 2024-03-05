a487061f838fcb5bf6236ed14d5c2104f76e884d
Tom Houlé <tom@tomhoule.com>
2024-03-05 11:53:00 +0100

    first relay style test passing

--- engine/crates/engine-v2/src/engine.rs
+++ engine/crates/engine-v2/src/engine.rs
@@ -141,12 +141,20 @@ impl Engine {
     ) -> Result<(), GraphqlError> {
         let enforce_trusted_documents = self.env.trusted_documents.trusted_documents_enabled();
         let persisted_query_extension = mem::take(&mut request.extensions.persisted_query);
+        let doc_id = mem::take(&mut request.operation_plan_cache_key.doc_id);
 
-        match (enforce_trusted_documents, persisted_query_extension) {
-            (true, None) => Err(GraphqlError::new("Only trusted document queries are accepted.")),
-            (true, Some(ext)) => self.handle_trusted_document_query(request, ext, client_name).await,
-            (false, None) => Ok(()),
-            (false, Some(ext)) => self.handle_apq(request, &ext).await,
+        match (enforce_trusted_documents, persisted_query_extension, doc_id) {
+            (true, None, None) => Err(GraphqlError::new("Only trusted document queries are accepted.")),
+            (true, Some(ext), _) => {
+                self.handle_apollo_client_style_trusted_document_query(request, ext, client_name)
+                    .await
+            }
+            (true, _, Some(document_id)) => {
+                self.handle_trusted_document_query(request, &document_id, client_name)
+                    .await
+            }
+            (false, None, _) => Ok(()),
+            (false, Some(ext), _) => self.handle_apq(request, &ext).await,
         }
     }
 
@@ -208,10 +216,32 @@ impl Engine {
         }
     }
 
-    async fn handle_trusted_document_query(
+    async fn handle_apollo_client_style_trusted_document_query(
         &self,
         request: &mut engine::Request,
         ext: PersistedQueryRequestExtension,
+        client_name: Option<&str>,
+    ) -> Result<(), GraphqlError> {
+        use std::fmt::Write;
+
+        let document_id = {
+            let mut id = String::with_capacity(ext.sha256_hash.len() * 2);
+
+            for byte in &ext.sha256_hash {
+                write!(id, "{byte:02x}").expect("write to String to succeed");
+            }
+
+            id
+        };
+
+        self.handle_trusted_document_query(request, &document_id, client_name)
+            .await
+    }
+
+    async fn handle_trusted_document_query(
+        &self,
+        request: &mut engine::Request,
+        document_id: &str,
         client_name: Option<&str>,
     ) -> Result<(), GraphqlError> {
         let Some(client_name) = client_name else {
@@ -220,16 +250,12 @@ impl Engine {
             )));
         };
 
-        let document_id: String = ext.sha256_hash.iter().map(|b| format!("{:02x}", b)).collect();
-
         match self.env.trusted_documents.fetch(client_name, &document_id).await {
-            Err(TrustedDocumentsError::RetrievalError(err)) => {
-                return Err(GraphqlError::new(format!(
-                    "Internal server error while fetching trusted document: {err}"
-                )))
-            }
+            Err(TrustedDocumentsError::RetrievalError(err)) => Err(GraphqlError::new(format!(
+                "Internal server error while fetching trusted document: {err}"
+            ))),
             Err(TrustedDocumentsError::DocumentNotFound) => {
-                return Err(GraphqlError::new(format!("Document id unknown: {document_id}")))
+                Err(GraphqlError::new(format!("Document id unknown: {document_id}")))
             }
             Ok(document_text) => {
                 request.operation_plan_cache_key.query = document_text;
--- engine/crates/integration-tests/tests/federation/trusted_documents.rs
+++ engine/crates/integration-tests/tests/federation/trusted_documents.rs
@@ -1,105 +1,159 @@
+use futures::Future;
 use gateway_v2::Gateway;
 use graphql_mocks::{FakeGithubSchema, MockGraphQlServer};
 use integration_tests::{
-    federation::{GatewayV2Ext, TestTrustedDocument},
+    engine::GraphQlRequest,
+    federation::{GatewayV2Ext, TestFederationGateway, TestTrustedDocument},
     runtime,
 };
 use serde_json::json;
 
-#[test]
-fn trusted_documents() {
+const TRUSTED_DOCUMENTS: &[TestTrustedDocument] = &[
+    TestTrustedDocument {
+        branch_id: "my-branch-id",
+        client_name: "ios-app",
+        document_id: "df40d7fae090cfec1c7e96d78ffb4087f0421798d96c4c90df3556c7de585dc9",
+        document_text: "query { serverVersion }",
+    },
+    TestTrustedDocument {
+        branch_id: "my-branch-id",
+        client_name: "ios-app",
+        document_id: "favourite-repo-query-doc-id",
+        document_text: "query { __typename }",
+    },
+    TestTrustedDocument {
+        branch_id: "my-branch-id",
+        client_name: "ios-app",
+        document_id: r#"
+            query { 
+                pullRequestsAndIssues(filter: { search: "1" }) { __typename } 
+                allBotPullRequests { __typename } 
+            }"#,
+        document_text: "",
+    },
+    TestTrustedDocument {
+        branch_id: "other-branch-id",
+        client_name: "ios-app",
+        document_id: "this-one-should-not-be-reachable-on-my-branch",
+        document_text: "query { serverVersion }",
+    },
+];
+
+fn test<Fn, Fut>(test_fn: Fn)
+where
+    Fn: FnOnce(TestFederationGateway) -> Fut,
+    Fut: Future<Output = ()>,
+{
     runtime().block_on(async move {
         let github_mock = MockGraphQlServer::new(FakeGithubSchema).await;
-
-        let trusted_documents = vec![
-            TestTrustedDocument {
-                branch_id: "my-branch-id",
-                client_name: "ios-app",
-                document_id: "df40d7fae090cfec1c7e96d78ffb4087f0421798d96c4c90df3556c7de585dc9",
-                document_text: "query { serverVersion }",
-            },
-            TestTrustedDocument {
-                branch_id: "my-branch-id",
-                client_name: "ios-app",
-                document_id: "favourite-repo-query-doc-id",
-                document_text: "query { serverVersion }",
-            },
-            TestTrustedDocument {
-                branch_id: "other-branch-id",
-                client_name: "ios-app",
-                document_id: "this-one-should-not-be-reachable-on-my-branch",
-                document_text: "query { serverVersion }",
-            },
-        ];
 
         let engine = Gateway::builder()
             .with_schema("schema", &github_mock)
             .await
-            .with_trusted_documents("my-branch-id".to_owned(), trusted_documents.clone())
+            .with_trusted_documents("my-branch-id".to_owned(), TRUSTED_DOCUMENTS.to_owned())
             .finish()
             .await;
 
-        let execute = |query: &'static str, headers: &[(&str, &str)], extensions: &serde_json::Value| {
-            let mut builder = engine.execute(query).extensions(extensions);
+        test_fn(engine).await
+    })
+}
 
-            for (header_name, header_value) in headers {
-                builder = builder.header(*header_name, *header_value);
-            }
-
-            builder
-        };
-
-        // Non-trusted-document queries are rejected.
-        {
-            let response = execute("query { serverVersion }", &[], &serde_json::Value::Null).await;
-
-            insta::assert_json_snapshot!(response, @r###"
-            {
-              "errors": [
-                {
-                  "message": "Only trusted document queries are accepted."
-                }
-              ]
-            }
-            "###);
-        }
-
-        // Trusted document queries without client name header are rejected
-        {
-            let response = execute(
-                "",
-                &[],
-                &json!({"persistedQuery": { "version": 1, "sha256Hash": &trusted_documents[0].document_id }}),
-            )
+#[test]
+fn relay_style_happy_path() {
+    test(|engine| async move {
+        let response = engine
+            .execute(GraphQlRequest {
+                query: String::new(),
+                operation_name: None,
+                variables: None,
+                extensions: None,
+                doc_id: Some(TRUSTED_DOCUMENTS[1].document_id.to_owned()),
+            })
+            .header("x-grafbase-client-name", "ios-app")
             .await;
 
-            insta::assert_json_snapshot!(response, @r###"
-            {
-              "errors": [
-                {
-                  "message": "Trusted document queries must include the x-grafbase-client-name header"
-                }
-              ]
-            }
-            "###)
-        }
-
-        // Apollo client style happy path
+        insta::assert_json_snapshot!(response, @r###"
         {
-            let response = execute(
-                "",
-                &[("x-grafbase-client-name", "ios-app")],
-                &json!({"persistedQuery": { "version": 1, "sha256Hash": &trusted_documents[0].document_id }}),
-            )
+          "data": {
+            "__typename": "Query"
+          }
+        }
+        "###)
+    })
+}
+
+#[test]
+fn apollo_client_style_happy_path() {
+    test(|engine| async move {
+        let response = engine
+            .execute("")
+            .extensions(&json!({"persistedQuery": { "version": 1, "sha256Hash": &TRUSTED_DOCUMENTS[0].document_id }}))
+            .header("x-grafbase-client-name", "ios-app")
             .await;
 
-            insta::assert_json_snapshot!(response, @r###"
+        insta::assert_json_snapshot!(response, @r###"
             {
               "data": {
                 "serverVersion": "1"
               }
             }
             "###)
-        }
-    });
+    })
 }
+
+// #[test]
+// fn trusted_documents() {
+//     test(|engine| async move {
+//         let execute = |query: &'static str, headers: &[(&str, &str)], extensions: &serde_json::Value| {
+//             let mut builder = engine.execute(query).extensions(extensions);
+
+//             for (header_name, header_value) in headers {
+//                 builder = builder.header(*header_name, *header_value);
+//             }
+
+//             builder
+//         };
+
+//         // Non-trusted-document queries are rejected.
+//         {
+//             let response = execute("query { serverVersion }", &[], &serde_json::Value::Null).await;
+
+//             insta::assert_json_snapshot!(response, @r###"
+//             {
+//               "errors": [
+//                 {
+//                   "message": "Only trusted document queries are accepted."
+//                 }
+//               ]
+//             }
+//             "###);
+//         }
+
+//         // Trusted document queries without client name header are rejected
+//         {
+//             let response = execute(
+//                 "",
+//                 &[],
+//                 &json!({"persistedQuery": { "version": 1, "sha256Hash": &trusted_documents[0].document_id }}),
+//             )
+//             .await;
+
+//             insta::assert_json_snapshot!(response, @r###"
+//             {
+//               "errors": [
+//                 {
+//                   "message": "Trusted document queries must include the x-grafbase-client-name header"
+//                 }
+//               ]
+//             }
+//             "###)
+//         }
+
+//         // Apollo client style happy path
+//         {}
+
+//         // Relay style happy path
+
+//         // TODO test with variables
+//     });
+// }
