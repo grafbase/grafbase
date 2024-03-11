b1e07362e4fceb773cca6f2872941c6c5e5429be
Julius de Bruijn <julius@nauk.io>
2024-03-11 14:35:06 +0100

    chore: log info when a gateway is created/updated (#1423)
    # Description
    
    We should log when we successfully created/updated a new gateway.
    
    # Type of change
    
    - [ ] 💔 Breaking
    - [ ] 🚀 Feature
    - [x] 🐛 Fix
    - [ ] 🛠️ Tooling
    - [ ] 🔨 Refactoring
    - [ ] 🧪 Test
    - [ ] 📦 Dependency
    - [ ] 📖 Requires documentation update

--- cli/crates/production-server/src/server/graph_fetch_method.rs
+++ cli/crates/production-server/src/server/graph_fetch_method.rs
@@ -6,6 +6,7 @@ use crate::config::{AuthenticationConfig, OperationLimitsConfig};
 use ascii::AsciiString;
 use std::sync::Arc;
 use tokio::sync::watch;
+use tracing::Level;
 
 /// The method of running the gateway.
 pub enum GraphFetchMethod {
@@ -62,6 +63,14 @@ impl GraphFetchMethod {
                 });
             }
             GraphFetchMethod::FromLocal { federated_schema } => {
+                tracing::event!(
+                    Level::INFO,
+                    message = "creating a new gateway",
+                    operation_limits = operation_limits.is_some(),
+                    introspection_enabled = enable_introspection,
+                    authentication = authentication.is_some(),
+                );
+
                 let gateway = gateway::generate(
                     &federated_schema,
                     operation_limits,
