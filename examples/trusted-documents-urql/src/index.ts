87d62dfd2a664b0d1cbbf7421c13accdbd1d002e
Tom Houlé <tom@tomhoule.com>
2024-03-18 09:39:56 +0100

    prettier

--- examples/trusted-documents-urql/src/index.ts
+++ examples/trusted-documents-urql/src/index.ts
@@ -1,43 +1,41 @@
-import { Client, fetchExchange, cacheExchange, gql } from 'urql';
-import { persistedExchange } from '@urql/exchange-persisted';
+import { Client, fetchExchange, cacheExchange, gql } from 'urql'
+import { persistedExchange } from '@urql/exchange-persisted'
 import manifest from '../persisted-query-manifest.json'
 
-const queryMap = manifest.operations.reduce((acc, item) => {
-  acc[item.body] = item.id;
-  return acc
-}, {} as { [key: string]: string })
-
+const queryMap = manifest.operations.reduce(
+  (acc, item) => {
+    acc[item.body] = item.id
+    return acc
+  },
+  {} as { [key: string]: string }
+)
 
 const planetsQuery = gql`
   query Planets {
     allPlanets {
       totalCount
-       edges {
-         node {
-            name
-            climates
-            population
-         }
-       }
+      edges {
+        node {
+          name
+          climates
+          population
+        }
+      }
     }
-  }`;
+  }
+`
 
 const peopleQuery = gql`
   query People($count: Int!) {
     allPeople(first: $count) {
-         edges {
-           node {
-             name
-           }
-         }
+      edges {
+        node {
+          name
+        }
+      }
     }
-
-
-
-
   }
-`;
-
+`
 
 const client = new Client({
   url: 'http://127.0.0.1:5000/graphql',
@@ -51,15 +49,17 @@ const client = new Client({
     persistedExchange({
       enableForMutation: true,
       enforcePersistedQueries: true,
-      generateHash: (query) => Promise.resolve(queryMap[query]),
+      generateHash: (query) => Promise.resolve(queryMap[query])
     }),
-    fetchExchange,
-  ],
-});
+    fetchExchange
+  ]
+})
 
 async function main() {
   const response = await client.query(peopleQuery, { count: 4 })
   console.log(JSON.stringify(response, null, 2))
 }
 
-main().catch(console.error).then(() => process.exit(1))
+main()
+  .catch(console.error)
+  .then(() => process.exit(1))
