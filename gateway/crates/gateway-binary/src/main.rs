bc8fa4c0cef8b51400c83b872759b8a188c74681
Julius de Bruijn <julius@nauk.io>
2024-03-19 19:28:38 +0100

    fix: back to normal otel collector for lambda

--- gateway/crates/gateway-binary/src/main.rs
+++ gateway/crates/gateway-binary/src/main.rs
@@ -32,8 +32,6 @@ fn main() -> anyhow::Result<()> {
 
 #[cfg(not(feature = "lambda"))]
 fn start_server(filter: EnvFilter, args: Args, mut config: Config) -> Result<(), anyhow::Error> {
-    // let (otel_layer, reload_handle) = grafbase_tracing::otel::layer::new_noop();
-
     use grafbase_tracing::otel::{layer, opentelemetry_sdk::runtime::Tokio};
 
     let (otel_layer, filter) = match config.telemetry.take() {
@@ -58,42 +56,22 @@ fn start_server(filter: EnvFilter, args: Args, mut config: Config) -> Result<(),
 }
 
 #[cfg(feature = "lambda")]
-fn start_server(filter: EnvFilter, args: Args, config: Config) -> Result<(), anyhow::Error> {
+fn start_server(filter: EnvFilter, args: Args, mut config: Config) -> Result<(), anyhow::Error> {
+    use grafbase_tracing::otel::layer;
     use grafbase_tracing::otel::opentelemetry::global;
-    use grafbase_tracing::otel::opentelemetry::trace::TracerProvider as _;
-    use grafbase_tracing::otel::opentelemetry_sdk::trace::TracerProvider;
+    use grafbase_tracing::otel::opentelemetry_sdk::runtime::Tokio;
     use opentelemetry_aws::trace::XrayPropagator;
 
     global::set_text_map_propagator(XrayPropagator::default());
 
-    let filter = config
-        .telemetry
-        .as_ref()
-        .map(|config| EnvFilter::new(&config.tracing.filter))
-        .unwrap_or(filter);
+    let (otel_layer, filter) = match config.telemetry.take() {
+        Some(config) => {
+            let env_filter = EnvFilter::new(&config.tracing.filter);
+            let otel_layer = layer::new_batched(&config.service_name, config.tracing, Tokio)?;
 
-    let otel_layer = match config
-        .telemetry
-        .as_ref()
-        .and_then(|config| config.tracing.exporters.stdout.as_ref())
-    {
-        Some(stdout_config) if stdout_config.enabled => {
-            let otel_service_name = config
-                .telemetry
-                .as_ref()
-                .map(|config| config.service_name.as_str())
-                .unwrap_or("grafbase-gateway");
-
-            let provider = TracerProvider::builder()
-                .with_simple_exporter(opentelemetry_stdout::SpanExporter::default())
-                .build();
-
-            let tracer = provider.tracer(otel_service_name.to_string());
-            let otel_layer = grafbase_tracing::otel::tracing_opentelemetry::layer().with_tracer(tracer);
-
-            Some(otel_layer)
+            (Some(otel_layer), env_filter)
         }
-        _ => None,
+        None => (None, filter),
     };
 
     tracing_subscriber::registry()
