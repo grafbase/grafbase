59a3b1c975d7e9feaa4e9534cb1856060bc67098
Julius de Bruijn <julius@nauk.io>
2024-03-19 17:02:11 +0100

    fix: get rid of otel-reload (now when we can do it)

--- gateway/crates/gateway-binary/src/main.rs
+++ gateway/crates/gateway-binary/src/main.rs
@@ -31,28 +31,38 @@ fn main() -> anyhow::Result<()> {
 }
 
 #[cfg(not(feature = "lambda"))]
-fn start_server(filter: EnvFilter, args: Args, config: Config) -> Result<(), anyhow::Error> {
-    let (otel_layer, reload_handle) = grafbase_tracing::otel::layer::new_noop();
+fn start_server(filter: EnvFilter, args: Args, mut config: Config) -> Result<(), anyhow::Error> {
+    // let (otel_layer, reload_handle) = grafbase_tracing::otel::layer::new_noop();
+
+    use grafbase_tracing::otel::{layer, opentelemetry_sdk::runtime::Tokio};
+
+    let (otel_layer, filter) = match config.telemetry.take() {
+        Some(config) => {
+            let env_filter = EnvFilter::new(&config.tracing.filter);
+            let otel_layer = layer::new_batched(&config.service_name, config.tracing, Tokio)?;
+
+            (Some(otel_layer), env_filter)
+        }
+        None => (None, filter),
+    };
 
     tracing_subscriber::registry()
-        .with(Some(otel_layer))
+        .with(otel_layer)
         .with(tracing_subscriber::fmt::layer())
         .with(filter)
         .init();
 
-    federated_server::start(args.listen_address, config, args.fetch_method()?, Some(reload_handle))?;
+    federated_server::start(args.listen_address, config, args.fetch_method()?)?;
 
     Ok(())
 }
 
 #[cfg(feature = "lambda")]
 fn start_server(filter: EnvFilter, args: Args, config: Config) -> Result<(), anyhow::Error> {
-    use grafbase_tracing::otel::layer::FilteredLayer;
     use grafbase_tracing::otel::opentelemetry::global;
     use grafbase_tracing::otel::opentelemetry::trace::TracerProvider as _;
     use grafbase_tracing::otel::opentelemetry_sdk::trace::TracerProvider;
     use opentelemetry_aws::trace::XrayPropagator;
-    use tracing_subscriber::{reload, Registry};
 
     let filter = config
         .telemetry
@@ -88,12 +98,7 @@ fn start_server(filter: EnvFilter, args: Args, config: Config) -> Result<(), any
 
     tracing_subscriber::registry().with(otel_layer).with(filter).init();
 
-    federated_server::start(
-        args.listen_address,
-        config,
-        args.fetch_method()?,
-        None::<reload::Handle<FilteredLayer<Registry>, Registry>>,
-    )?;
+    federated_server::start(args.listen_address, config, args.fetch_method()?)?;
 
     Ok(())
 }
