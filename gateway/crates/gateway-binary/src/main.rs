7267b669474a70da13e270647d72d83ed8334817
Julius de Bruijn <julius@nauk.io>
2024-03-19 12:51:35 +0100

    feat: propagate x-ray trace ids for lambda

--- gateway/crates/gateway-binary/src/main.rs
+++ gateway/crates/gateway-binary/src/main.rs
@@ -2,12 +2,8 @@
 
 use args::Args;
 use clap::Parser;
-use grafbase_tracing::otel::layer::BoxedLayer;
 use mimalloc::MiMalloc;
-use tracing_core::Subscriber;
-use tracing_subscriber::registry::LookupSpan;
 use tracing_subscriber::util::SubscriberInitExt;
-use tracing_subscriber::Layer;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter};
 
 #[global_allocator]
@@ -23,11 +19,19 @@ fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
     let filter = EnvFilter::builder().parse_lossy(args.log_filter());
+
+    start_server(filter, args)?;
+
+    Ok(())
+}
+
+#[cfg(not(feature = "lambda"))]
+fn start_server(filter: EnvFilter, args: Args) -> Result<(), anyhow::Error> {
     let (otel_layer, reload_handle) = grafbase_tracing::otel::layer::new_noop();
 
     tracing_subscriber::registry()
         .with(Some(otel_layer))
-        .with(log_format_layer())
+        .with(tracing_subscriber::fmt::layer())
         .with(filter)
         .init();
 
@@ -36,18 +40,22 @@ fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-#[cfg(not(feature = "lambda"))]
-fn log_format_layer<S>() -> BoxedLayer<S>
-where
-    S: Subscriber + for<'a> LookupSpan<'a>,
-{
-    tracing_subscriber::fmt::layer().boxed()
-}
-
 #[cfg(feature = "lambda")]
-fn log_format_layer<S>() -> BoxedLayer<S>
-where
-    S: Subscriber + for<'a> LookupSpan<'a>,
-{
-    tracing_subscriber::fmt::layer().json().boxed()
+fn start_server(filter: EnvFilter, args: Args) -> Result<(), anyhow::Error> {
+    use grafbase_tracing::otel::opentelemetry::global;
+    use opentelemetry_aws::trace::XrayPropagator;
+
+    global::set_text_map_propagator(XrayPropagator::default());
+
+    let (otel_layer, reload_handle) = grafbase_tracing::otel::layer::new_noop();
+
+    tracing_subscriber::registry()
+        .with(Some(otel_layer))
+        .with(tracing_subscriber::fmt::layer().json())
+        .with(filter)
+        .init();
+
+    federated_server::start(args.listen_address, &args.config, args.fetch_method()?, reload_handle)?;
+
+    Ok(())
 }
