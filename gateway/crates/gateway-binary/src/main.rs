da9472a74d0413cc06133412a40ef2a370985b5d
Julius de Bruijn <julius@nauk.io>
2024-03-19 15:35:15 +0100

    chore: release fourteenth rc

--- gateway/crates/gateway-binary/src/main.rs
+++ gateway/crates/gateway-binary/src/main.rs
@@ -35,27 +35,46 @@ fn start_server(filter: EnvFilter, args: Args) -> Result<(), anyhow::Error> {
         .with(filter)
         .init();
 
-    federated_server::start(args.listen_address, &args.config, args.fetch_method()?, reload_handle)?;
+    federated_server::start(
+        args.listen_address,
+        &args.config,
+        args.fetch_method()?,
+        Some(reload_handle),
+    )?;
 
     Ok(())
 }
 
 #[cfg(feature = "lambda")]
 fn start_server(filter: EnvFilter, args: Args) -> Result<(), anyhow::Error> {
+    use grafbase_tracing::otel::layer::FilteredLayer;
     use grafbase_tracing::otel::opentelemetry::global;
+    use grafbase_tracing::otel::opentelemetry::trace::TracerProvider as _;
+    use grafbase_tracing::otel::opentelemetry_sdk::trace::TracerProvider;
     use opentelemetry_aws::trace::XrayPropagator;
+    use tracing_subscriber::{reload, Registry};
 
     global::set_text_map_propagator(XrayPropagator::default());
 
-    let (otel_layer, reload_handle) = grafbase_tracing::otel::layer::new_noop();
+    let provider = TracerProvider::builder()
+        .with_simple_exporter(opentelemetry_stdout::SpanExporter::default())
+        .build();
+
+    let tracer = provider.tracer("grafbase-gateway");
+
+    let otel_layer = grafbase_tracing::otel::tracing_opentelemetry::layer().with_tracer(tracer);
 
     tracing_subscriber::registry()
         .with(Some(otel_layer))
-        .with(tracing_subscriber::fmt::layer().with_ansi(false))
         .with(filter)
         .init();
 
-    federated_server::start(args.listen_address, &args.config, args.fetch_method()?, reload_handle)?;
+    federated_server::start(
+        args.listen_address,
+        &args.config,
+        args.fetch_method()?,
+        None::<reload::Handle<FilteredLayer<Registry>, Registry>>,
+    )?;
 
     Ok(())
 }
