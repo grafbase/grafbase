60df8c69699784b1aa3252c3d3bf464939040fa3
Benjamin Rabier <benjamin+git@rabier.dev>
2024-04-09 15:21:29 +0200

    Add basic request metrics

--- engine/crates/tracing/src/tower.rs
+++ engine/crates/tracing/src/tower.rs
@@ -6,6 +6,8 @@ use tower_http::classify::{ServerErrorsAsFailures, ServerErrorsFailureClass, Sha
 use tower_http::trace::{DefaultOnBodyChunk, DefaultOnEos, DefaultOnRequest};
 use tracing::Span;
 
+use crate::span::GRAFBASE_TARGET;
+
 /// A [tower_http::trace::TraceLayer] that creates [crate::span::request::HttpRequestSpan] for each incoming request
 /// and records request and response attributes in the span
 pub fn layer<B: Body>() -> tower_http::trace::TraceLayer<
@@ -19,14 +21,43 @@ pub fn layer<B: Body>() -> tower_http::trace::TraceLayer<
 > {
     tower_http::trace::TraceLayer::new_for_http()
         .make_span_with(crate::span::request::MakeHttpRequestSpan)
-        .on_response(|response: &Response<_>, _latency: Duration, span: &Span| {
+        .on_response(|response: &Response<_>, latency: Duration, span: &Span| {
             use crate::span::HttpRecorderSpanExt;
 
+            let cache_status = response
+                .headers()
+                .get("x-grafbase-cache")
+                .and_then(|value| value.to_str().ok())
+                .unwrap_or_default();
+            tracing::event!(
+                target: GRAFBASE_TARGET,
+                tracing::Level::INFO,
+                counter.request_count = 1,
+                http.response.status_code = response.status().as_u16(),
+                http.response.headers.cache_status = cache_status
+            );
+            tracing::event!(
+                target: GRAFBASE_TARGET,
+                tracing::Level::INFO,
+                histogram.latency = latency.as_millis(),
+            );
             span.record_response(response);
         })
         .on_failure(|error: ServerErrorsFailureClass, _latency: Duration, span: &Span| {
             use crate::span::HttpRecorderSpanExt;
 
+            let status = match error {
+                ServerErrorsFailureClass::StatusCode(code) => code.as_u16(),
+                ServerErrorsFailureClass::Error(_) => 500,
+            };
+            tracing::event!(
+                target: GRAFBASE_TARGET,
+                tracing::Level::INFO,
+                counter.request = 1,
+                http.response.status_code = status,
+                http.response.headers.cache_stauts = "BYPASS"
+
+            );
             span.record_failure(error.to_string().as_str());
         })
 }
