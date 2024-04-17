117c9637956efd87a7b3e6c7b50c073014b71645
Graeme Coupar <graeme@turbofish.tech>
2024-04-17 14:04:41 +0100

    more benchmark tweaks

--- engine/crates/engine-benchmarks/benches/memory-test.rs
+++ engine/crates/engine-benchmarks/benches/memory-test.rs
@@ -2,13 +2,13 @@
 
 use std::future::IntoFuture;
 
-use divan::AllocProfiler;
+// use divan::AllocProfiler;
 
 use engine_benchmarks::MockProcess;
 use integration_tests::{EngineBuilder, ResponseExt};
 
-#[global_allocator]
-static ALLOC: AllocProfiler = AllocProfiler::system();
+// #[global_allocator]
+// static ALLOC: AllocProfiler = AllocProfiler::system();
 
 fn main() {
     divan::main();
@@ -19,24 +19,29 @@ fn ton_of_data_test(bencher: divan::Bencher<'_, '_>) {
     let mock = MockProcess::new();
     let port = mock.port;
 
+    let runtime = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+    let engine = runtime.block_on(EngineBuilder::new(schema(port, true)).build());
+
     bencher
         .with_inputs(|| {
+            // Make a new runtime per test to make sure nothing interferes
             let runtime = tokio::runtime::Builder::new_current_thread()
                 .enable_all()
                 .build()
                 .unwrap();
 
-            let engine = runtime.block_on(EngineBuilder::new(schema(port, true)).build());
-
-            (runtime, engine)
+            runtime
         })
-        .bench_values(|(runtime, engine)| {
+        .bench_values(|runtime| {
             let result = runtime.block_on(engine.execute(QUERY).into_future());
 
             let result = result.assert_success();
 
             // Return all our inputs so their Drop doesn't get counted in our bench
-            (result, engine, runtime)
+            (result, runtime)
         });
 }
 
