ac1921a30086bdc8ede0d68d1fb44ed035067e8d
Graeme Coupar <graeme@grafbase.com>
2024-05-15 13:44:20 +0100

    fix: remove to_plural from postgres connector runtime (#1718)
    I was digging into the binary sizes of the gateway & executor last week.
    One of the things I noticed was that the regex crate (a known space
    waster) was being pulled into the executor, so I dug into that. Turns
    out this was from a single innocent looking call to
    `Inflector::to_plural` in the postgres connector.
    
    So, this works the connector to do that call at deploy time in the
    postgres-parser instead of at runtime, which should reduce the executor
    payload by a fair bit.
    
    I've approached this by just storing the singular & plural relation
    field names against every table and storing that. I'm not 100% happy
    with this implementation: these are technically only required for tables
    with relations, and even then a given relation might only need one of
    the forms. But I'm not familiar enough with the code to figure out the
    best place to do _exactly_ what we need. Particularly since tables names
    can change later in the process if their name clashes, so we'd then need
    to propagate that into our memoised relation names. If anyone knows of a
    better way to do this (or wants to revisit) feel free.

--- engine/crates/postgres-connector-types/src/database_definition/table.rs
+++ engine/crates/postgres-connector-types/src/database_definition/table.rs
@@ -8,6 +8,8 @@ pub struct Table<T> {
     pub(super) schema_id: SchemaId,
     pub(super) database_name: T,
     pub(super) client_name: T,
+    pub(super) client_field_name: T,
+    pub(super) client_field_name_plural: T,
 }
 
 impl<T> Copy for Table<T> where T: Copy {}
@@ -20,16 +22,28 @@ impl<T> Table<T> {
     pub(super) fn set_client_name(&mut self, client_name: T) {
         self.client_name = client_name;
     }
+
+    pub(super) fn set_client_field_name(&mut self, client_field_name: T) {
+        self.client_field_name = client_field_name;
+    }
+
+    pub(super) fn set_client_field_name_plural(&mut self, client_field_name_plural: T) {
+        self.client_field_name_plural = client_field_name_plural;
+    }
 }
 
 impl Table<String> {
     pub fn new(schema_id: SchemaId, name: String) -> Self {
         let client_name = name.to_pascal_case();
+        let client_field_name = client_name.to_camel_case();
+        let client_field_name_plural = client_field_name.to_plural();
 
         Self {
             schema_id,
             database_name: name,
             client_name,
+            client_field_name,
+            client_field_name_plural,
         }
     }
 
@@ -40,14 +54,30 @@ impl Table<String> {
     pub(crate) fn client_name(&self) -> &str {
         &self.client_name
     }
+
+    pub(crate) fn client_field_name(&self) -> &str {
+        &self.client_field_name
+    }
+
+    pub(crate) fn client_field_name_plural(&self) -> &str {
+        &self.client_field_name_plural
+    }
 }
 
 impl Table<StringId> {
-    pub fn new(schema_id: SchemaId, database_name: StringId, client_name: StringId) -> Self {
+    pub fn new(
+        schema_id: SchemaId,
+        database_name: StringId,
+        client_name: StringId,
+        client_field_name: StringId,
+        client_field_name_plural: StringId,
+    ) -> Self {
         Self {
             schema_id,
             database_name,
             client_name,
+            client_field_name,
+            client_field_name_plural,
         }
     }
 
@@ -57,5 +87,13 @@ impl Table<StringId> {
 
     pub(crate) fn client_name(&self) -> StringId {
         self.client_name
+    }
+
+    pub(crate) fn client_field_name(&self) -> StringId {
+        self.client_field_name
+    }
+
+    pub(crate) fn client_field_name_plural(&self) -> StringId {
+        self.client_field_name_plural
     }
 }
