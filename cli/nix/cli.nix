39942b73af46e4dafb659c9abb7300104d8ec064
Tom Houlé <tom@tomhoule.com>
2024-05-17 12:18:01 +0200

    nix: remove cli-app

--- cli/nix/cli.nix
+++ cli/nix/cli.nix
@@ -1,10 +1,10 @@
-{
-  pkgs,
-  crane,
-  lib,
-  config,
-  ...
-}: let
+{ pkgs
+, crane
+, lib
+, config
+, ...
+}:
+let
   rustToolchain = pkgs.rust-bin.stable.latest.default;
   craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
   workspaceRoot = builtins.path {
@@ -42,18 +42,19 @@
     !/packages/grafbase-sdk/package.json
   '';
 
-  src = pkgs.nix-gitignore.gitignoreSource [extraIgnores] (lib.cleanSourceWith {
+  src = pkgs.nix-gitignore.gitignoreSource [ extraIgnores ] (lib.cleanSourceWith {
     filter = lib.cleanSourceFilter;
     src = workspaceRoot;
   });
 
-  version = pkgs.runCommand "getVersion" {} ''
+  version = pkgs.runCommand "getVersion" { } ''
     ${pkgs.dasel}/bin/dasel \
       --file ${../../Cargo.toml} \
       --selector workspace.package.version\
       --write - | tr -d "\n" > $out
   '';
-in {
+in
+{
   imports = [
     ./wrappers.nix
   ];
@@ -71,7 +72,6 @@ in {
       "-C linker=clang -C link-arg=-fuse-ld=lld"
     ];
 
-    GRAFBASE_CLI_PATHFINDER_BUNDLE_PATH = config.packages.cli-app;
     GRAFBASE_CLI_WRAPPERS_PATH = config.packages.wrappers;
 
     doCheck = false;
