ca4121ebd8485c1babb8250202230361ba2abf53
Hugo Barrigas <hfgbarrigas@gmail.com>
2024-06-27 13:21:04 +0100

    feat: cli in memory rate limiting (#1816)
    # Description
    
    Adds in memory rate limiting for local dev through the CLI.
    
    # Type of change
    
    - [ ] 💔 Breaking
    - [x] 🚀 Feature
    - [ ] 🐛 Fix
    - [ ] 🛠️ Tooling
    - [ ] 🔨 Refactoring
    - [ ] 🧪 Test
    - [ ] 📦 Dependency
    - [ ] 📖 Requires documentation update

--- engine/crates/runtime-local/src/lib.rs
+++ engine/crates/runtime-local/src/lib.rs
@@ -1,19 +1,20 @@
 mod bridge;
 mod cache;
 mod fetch;
+#[cfg(feature = "wasi")]
+mod hooks;
 mod kv;
 mod log;
 mod pg;
+mod rate_limiting;
 mod ufd_invoker;
-
-#[cfg(feature = "wasi")]
-mod hooks;
 
 pub use bridge::Bridge;
 pub use cache::InMemoryCache;
 pub use fetch::NativeFetcher;
 pub use kv::*;
 pub use pg::{LazyPgConnectionsPool, LocalPgTransportFactory};
+pub use rate_limiting::InMemoryRateLimiting;
 pub use ufd_invoker::UdfInvokerImpl;
 
 #[cfg(feature = "wasi")]
