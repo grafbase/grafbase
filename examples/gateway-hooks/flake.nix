5b3ee437577d1e84aa1e2090f396c4d73215bea4
Julius de Bruijn <julius@nauk.io>
2024-07-19 18:14:34 +0200

    chore: update hooks defs and example

--- examples/gateway-hooks/flake.nix
+++ examples/gateway-hooks/flake.nix
@@ -4,7 +4,7 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/master";
   inputs.devshell.url = "github:numtide/devshell";
   inputs.flake-parts.url = "github:hercules-ci/flake-parts";
-  inputs.grafbase.url = "github:grafbase/grafbase/eb14b9bd633ba0079dabb658aa52c49d71d6a2d5";
+  inputs.grafbase.url = "github:grafbase/grafbase/gateway-0.6.1";
 
   outputs = inputs @ {
     self,
