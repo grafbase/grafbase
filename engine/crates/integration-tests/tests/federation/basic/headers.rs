e8ca0d5e629997ea4f69ffdc909b5239e83f8d3a
Julius de Bruijn <julius@nauk.io>
2024-07-22 09:09:23 +0200

    chore: add renaming test for forward header rule

--- engine/crates/integration-tests/tests/federation/basic/headers.rs
+++ engine/crates/integration-tests/tests/federation/basic/headers.rs
@@ -292,6 +292,48 @@ fn test_regex_header_forwarding() {
 }
 
 #[test]
+fn test_header_forwarding_with_rename() {
+    let response = runtime().block_on(async move {
+        let github_mock = MockGraphQlServer::new(FakeGithubSchema).await;
+
+        let engine = Engine::builder()
+            .with_subgraph("github", &github_mock)
+            .with_header_rule(SubgraphHeaderRule::Forward(SubgraphHeaderForward {
+                name: NameOrPattern::Name(String::from("x-source")),
+                rename: Some(String::from("y-source")),
+                default: None,
+            }))
+            .build()
+            .await;
+
+        engine
+            .execute("query { headers { name value }}")
+            .header("x-source", "boom")
+            .await
+    });
+
+    let mut response: Response = serde_json::from_value(response.into_data()).unwrap();
+    response.headers.sort();
+
+    insta::assert_debug_snapshot!(response.headers, @r###"
+    [
+        Header {
+            name: "accept",
+            value: "*/*",
+        },
+        Header {
+            name: "content-type",
+            value: "application/json",
+        },
+        Header {
+            name: "y-source",
+            value: "boom",
+        },
+    ]
+    "###);
+}
+
+#[test]
 fn test_header_forwarding_with_default() {
     let response = runtime().block_on(async move {
         let echo_subgraph = MockGraphQlServer::new(EchoSchema).await;
