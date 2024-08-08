8da82bf6db1d8d7575051882f04a574bdc67f5e1
Tom Houlé <tom@tomhoule.com>
2024-08-08 12:22:06 +0200

    cli: rename project to graph in BranchRef
    For consistency.

--- cli/crates/cli/src/cli_input/branch_ref.rs
+++ cli/crates/cli/src/cli_input/branch_ref.rs
@@ -1,23 +1,22 @@
 use std::{fmt, str};
 
-/// Parsed project reference. A project reference is a string of the form `account/project@branch`.
+/// Parsed graph reference. A graph reference is a string of the form `account/graph@branch`.
 #[derive(Clone, Debug)]
 pub struct BranchRef {
     account: String,
-    project: String,
+    graph: String,
     branch: String,
 }
 
 impl BranchRef {
-    pub(crate) const ARG_DESCRIPTION: &'static str =
-        r#"Branch reference following the format "account/project@branch""#;
+    pub(crate) const ARG_DESCRIPTION: &'static str = r#"Branch reference following the format "account/graph@branch""#;
 
     pub(crate) fn account(&self) -> &str {
         self.account.as_ref()
     }
 
-    pub(crate) fn project(&self) -> &str {
-        self.project.as_ref()
+    pub(crate) fn graph(&self) -> &str {
+        self.graph.as_ref()
     }
 
     pub(crate) fn branch(&self) -> &str {
@@ -29,7 +28,8 @@ impl str::FromStr for BranchRef {
     type Err = &'static str;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        const GENERIC_ERR: &str = r#"Invalid branch reference. The branch reference argument must follow the format: "account/project@branch""#;
+        const GENERIC_ERR: &str =
+            r#"Invalid branch reference. The branch reference argument must follow the format: "account/graph@branch""#;
 
         let Some((account, rest)) = s.split_once('/') else {
             return Err(GENERIC_ERR);
@@ -39,12 +39,12 @@ impl str::FromStr for BranchRef {
             return Err("The account name is missing before '/'.");
         }
 
-        let Some((project, branch)) = rest.split_once('@') else {
+        let Some((graph, branch)) = rest.split_once('@') else {
             return Err(GENERIC_ERR);
         };
 
-        if project.is_empty() {
-            return Err("The project name is missing.");
+        if graph.is_empty() {
+            return Err("The graph name is missing.");
         }
 
         if branch.is_empty() {
@@ -53,7 +53,7 @@ impl str::FromStr for BranchRef {
 
         Ok(BranchRef {
             account: account.to_owned(),
-            project: project.to_owned(),
+            graph: graph.to_owned(),
             branch: branch.to_owned(),
         })
     }
@@ -63,7 +63,7 @@ impl fmt::Display for BranchRef {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.write_str(&self.account)?;
         f.write_str("/")?;
-        f.write_str(&self.project)?;
+        f.write_str(&self.graph)?;
         f.write_str("@")?;
         f.write_str(&self.branch)?;
 
@@ -79,8 +79,8 @@ mod tests {
     fn branch_ref_ok() {
         let cases = [
             "microsoft/windows@main",
-            "test/project@master",
-            "__my__/_____project-with-things@branch-here",
+            "test/graph@master",
+            "__my__/_____graph-with-things@branch-here",
             "1/2@3",
         ];
 
