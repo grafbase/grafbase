610b5a996cf6298ec12c72fe21d030d684f10260
Julius de Bruijn <julius@nauk.io>
2024-08-15 08:58:51 +0200

    chore: add test for subgraph response body size

--- gateway/crates/gateway-binary/tests/telemetry/metrics/operation/subgraph.rs
+++ gateway/crates/gateway-binary/tests/telemetry/metrics/operation/subgraph.rs
@@ -58,8 +58,8 @@ fn request_duration() {
 }
 
 #[test]
-fn request_body_size() {
-    with_gateway(|service_name, start_time_unix, gateway, clickhouse| async move {
+fn body_size() {
+    with_small_subgraph("", |service_name, start_time_unix, gateway, clickhouse| async move {
         let response = gateway
             .gql::<serde_json::Value>("query Simple { me { id } }")
             .send()
@@ -67,18 +67,11 @@ fn request_body_size() {
 
         insta::assert_json_snapshot!(response, @r###"
         {
-          "data": null,
-          "errors": [
-            {
-              "message": "Request to subgraph 'accounts' failed with: error sending request for url (http://127.0.0.1:46697/)",
-              "path": [
-                "me"
-              ],
-              "extensions": {
-                "code": "SUBGRAPH_REQUEST_ERROR"
-              }
+          "data": {
+            "me": {
+              "id": "1"
             }
-          ]
+          }
         }
         "###);
 
@@ -92,6 +85,31 @@ fn request_body_size() {
                 WHERE ServiceName = ? AND StartTimeUnix >= ?
                     AND ScopeName = 'grafbase'
                     AND MetricName = 'graphql.subgraph.request.body.size'
+                "#,
+            )
+            .bind(&service_name)
+            .bind(start_time_unix)
+            .fetch_one::<ExponentialHistogramRow>()
+            .await
+            .unwrap();
+
+        insta::assert_json_snapshot!(row, @r###"
+        {
+          "Count": 1,
+          "Attributes": {
+            "graphql.subgraph.name": "accounts"
+          }
+        }
+        "###);
+
+        let row = clickhouse
+            .query(
+                r#"
+                SELECT Count, Attributes
+                FROM otel_metrics_exponential_histogram
+                WHERE ServiceName = ? AND StartTimeUnix >= ?
+                    AND ScopeName = 'grafbase'
+                    AND MetricName = 'graphql.subgraph.response.body.size'
                 "#,
             )
             .bind(&service_name)
