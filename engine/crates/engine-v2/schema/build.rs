9b2a0335aef3ad06faf7c70a4d593bf8b3164aa0
Graeme Coupar <graeme@turbofish.tech>
2024-08-30 12:51:32 +0100

    chore: remove built
    we use the crate built to embed git hash & build time info into the
    engine-v2-schema crate.  However, this pulls in libgit2 which appears to be the
    #2 consumer of compile time on every platform on CI. 
    
    Specifically:
    
    - Windows spends a full 2 minutes compiling libgit2
    - aarch-darwin spends 40 seconds compiling libgit2
    - x86-64 linux spends 20 seconds
    
    I can't be bothered getting the rest of them, but you get the idea.  It's
    pretty trivial to get this info without building libgit2, so this PR does that.

--- engine/crates/engine-v2/schema/build.rs
+++ engine/crates/engine-v2/schema/build.rs
@@ -1,3 +1,45 @@
-fn main() {
-    built::write_built_file().expect("Failed to acquire build-time information");
+use std::{fmt::Write, process::Command};
+
+fn main() -> anyhow::Result<()> {
+    // Note: the built crate puportes to do this, but it pulls in libgit2
+    // for git info which adds minutes onto build time on windows at least,
+    // and just seems overkill for what we need
+    let mut output = String::new();
+
+    writeln!(
+        &mut output,
+        r#"pub const BUILT_TIME_UTC: &str = "{}";"#,
+        chrono::Utc::now().to_rfc2822()
+    )?;
+
+    write!(&mut output, r#"pub const GIT_COMMIT_HASH: Option<&str> = "#)?;
+
+    match get_current_git_hash() {
+        Some(git_hash) => {
+            writeln!(&mut output, r#"Some("{}");"#, git_hash)?;
+        }
+        None => {
+            writeln!(&mut output, "None;")?;
+        }
+    }
+
+    std::fs::write(format!("{}/built.rs", std::env::var("OUT_DIR")?), output)?;
+
+    Ok(())
+}
+
+fn get_current_git_hash() -> Option<String> {
+    let output = Command::new("git")
+        .current_dir(std::env::var("CARGO_MANIFEST_DIR").unwrap())
+        .args(["rev-parse", "HEAD"])
+        .output()
+        .inspect_err(|e| eprintln!("Could not run git: {e}"))
+        .ok()?;
+
+    if !output.status.success() {
+        eprintln!("Git returned status {}", output.status);
+        return None;
+    }
+
+    Some(std::str::from_utf8(&output.stdout).unwrap().trim().to_string())
 }
