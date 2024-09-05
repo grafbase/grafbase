f79967f8ab8c670819312616cc9eea486151a598
Graeme Coupar <graeme@grafbase.com>
2024-09-05 15:24:11 +0100

    Apply suggestions from code review

--- engine/crates/graphql-schema-diff/src/lib.rs
+++ engine/crates/graphql-schema-diff/src/lib.rs
@@ -16,8 +16,6 @@ use self::state::*;
 use cynic_parser::type_system as ast;
 use std::collections::HashMap;
 
-// TODO: Remove me (this is just to pretend there is a change)
-
 /// Diff two GraphQL schemas.
 pub fn diff(source: &str, target: &str) -> Result<Vec<Change>, cynic_parser::Error> {
     let [source, target] = [source, target].map(|sdl| -> Result<_, cynic_parser::Error> {
