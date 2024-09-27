5338b260cfb2cb08a9ee9cc8f529abc5855dc2af
Graeme Coupar <graeme@turbofish.tech>
2024-09-27 17:51:04 +0100

    update grafbase for cynic parser value changes

--- engine/crates/federated-graph/src/from_sdl/arguments.rs
+++ engine/crates/federated-graph/src/from_sdl/arguments.rs
@@ -1,11 +1,13 @@
 use super::ast;
 
 pub(super) trait GetArgumentsExt<'a> {
-    fn get_argument(&self, argument_name: &str) -> Option<ast::Argument<'a>>;
+    fn get_argument(&self, argument_name: &str) -> Option<cynic_parser::values::Value<'a>>;
 }
 
 impl<'a> GetArgumentsExt<'a> for ast::Directive<'a> {
-    fn get_argument(&self, argument_name: &str) -> Option<ast::Argument<'a>> {
-        self.arguments().find(|arg| arg.name() == argument_name)
+    fn get_argument(&self, argument_name: &str) -> Option<cynic_parser::values::Value<'a>> {
+        self.arguments()
+            .find(|arg| arg.name() == argument_name)
+            .map(|arg| arg.value())
     }
 }
