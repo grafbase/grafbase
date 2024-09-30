7da7c218a67139d8be355f78a11c06a34fa39642
Julius de Bruijn <julius@nauk.io>
2024-09-30 14:15:40 +0200

    fix(tracing): remove extra instrumentation spamming the traces

--- engine/crates/engine-v2/src/execution/hooks/subgraph.rs
+++ engine/crates/engine-v2/src/execution/hooks/subgraph.rs
@@ -1,11 +1,9 @@
 use http::HeaderMap;
 use runtime::hooks::{Hooks, SubgraphHooks};
-use tracing::{instrument, Level};
 
 use crate::response::GraphqlError;
 
 impl<'ctx, H: Hooks> super::RequestHooks<'ctx, H> {
-    #[instrument(skip_all, ret(level = Level::DEBUG))]
     pub async fn on_subgraph_request(
         &self,
         subgraph_name: &str,
