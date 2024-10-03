cc63bbdd049a855db1e9ecd0fffeef71f6128fa1
Graeme Coupar <graeme@turbofish.tech>
2024-10-03 13:08:53 -0400

    more updates

--- engine/crates/federated-graph/src/from_sdl/arguments.rs
+++ engine/crates/federated-graph/src/from_sdl/arguments.rs
@@ -1,11 +1,11 @@
 use super::ast;
 
 pub(super) trait GetArgumentsExt<'a> {
-    fn get_argument(&self, argument_name: &str) -> Option<cynic_parser::values::Value<'a>>;
+    fn get_argument(&self, argument_name: &str) -> Option<cynic_parser::values::ConstValue<'a>>;
 }
 
 impl<'a> GetArgumentsExt<'a> for ast::Directive<'a> {
-    fn get_argument(&self, argument_name: &str) -> Option<cynic_parser::values::Value<'a>> {
+    fn get_argument(&self, argument_name: &str) -> Option<cynic_parser::values::ConstValue<'a>> {
         self.arguments()
             .find(|arg| arg.name() == argument_name)
             .map(|arg| arg.value())
