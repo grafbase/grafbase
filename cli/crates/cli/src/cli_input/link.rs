5b92bdede2a789ae03b143a819cee383c7cca411
Yoav Lavi <yoav@yoavlavi.com>
2024-10-10 16:45:03 +0300

    Cleanup

--- cli/crates/cli/src/cli_input/link.rs
+++
@@ -1,8 +0,0 @@
-use ulid::Ulid;
-
-#[derive(Debug, clap::Args)]
-pub struct LinkCommand {
-    /// The id of the linked project
-    #[arg(short, long, value_name = "PROJECT_ID")]
-    pub project: Option<Ulid>,
-}
