0cb5f7ec5c7d37bde6a8c449615fc541938f1a24
Benjamin Rabier <benjamin+git@rabier.dev>
2024-10-21 11:28:56 +0200

    fix

--- engine/crates/engine-v2/query-planning/src/graph/cost_estimation.rs
+++ engine/crates/engine-v2/query-planning/src/graph/cost_estimation.rs
@@ -31,45 +31,17 @@ pub(crate) struct OriginRootAncestorId(NonZero<u32>);
 #[derive(id_derives::IndexedFields)]
 pub(crate) struct CostEstimator {
     // Ordered by EdgeIndex
-    edge_to_impacted_requirement: Vec<(EdgeIndex, RequirementId)>,
+    edge_to_impacted_requirement: Vec<(EdgeIndex, NodeIndex)>,
     #[indexed_by(EdgeCostId)]
     edge_cost: Vec<Cost>,
-    #[indexed_by(RequirementId)]
-    requirements: Vec<Requirement>,
-    #[indexed_by(AncestorId)]
-    all_ancestors: Vec<Ancestor>,
-    #[indexed_by(OriginRootAncestorId)]
-    all_origin_root_ancestors: Vec<OriginRootAncestor>,
-}
-
-pub(crate) struct OriginRootAncestor {
-    origin_query_field_ix: NodeIndex,
-    ancestor_id: AncestorId,
-}
-
-pub(crate) struct Requirement {
-    query_field_ix: NodeIndex,
-    // Ordering by NodeIndex
-    origin_root_ancestor_ids: IdRange<OriginRootAncestorId>,
-    // In topological order
-    ancestry_ids: IdRange<AncestorId>,
-}
-
-pub(crate) struct Ancestor {
-    parent: AncestorId,
-    edge_cost_id: EdgeCostId,
-    cumulative_cost_to_reach_requirement: Cost,
 }
 
 impl<'ctx, Op: Operation> OperationGraphBuilder<'ctx, Op> {
     #[allow(unused)]
     pub(crate) fn build_cost_estimator(&mut self) -> crate::Result<CostEstimator> {
         let mut estimator = CostEstimator {
-            edge_cost: vec![0; self.edge_cost_count() + 1],
+            edge_cost: vec![0; self.edge_cost_count()],
             edge_to_impacted_requirement: Vec::new(),
-            requirements: Vec::new(),
-            all_ancestors: Vec::new(),
-            all_origin_root_ancestors: Vec::new(),
         };
 
         estimator.populate_from(&mut self.graph);
@@ -87,15 +59,50 @@ impl CostEstimator {
     // - compute ancestors we need to keep track off for _all_ requirements.
     // - can't really merge requirements with parent as individual fields have a specific cost or
     // may be coming from entity join.
+    // - need all requirements in all cases. Don't need a max.
+    // - so distinct edgecost wouldn't work because some paths might not be taken.
+    // - the "simple" solution is to go over all fields???
     // - remove any requirement with a parent edge/node already in it. Cumulate all edgecostid?
     // kkproblem with edge cost id, union find actually
     // - remove any ancestor with parent already in it. It means there is a requirement that is a
     // parent of us, so
     // - end up with a list of (ancestor, requirement id)
+    //
+    // Ideal goal:
+    // - needs fields from 2 different subgraphs -> cost of 2
+    // - need nested field from a different subgraph -> cost of 2
+    // - only need fields from a single subgraph -> 1
+    // - only parent -> 0
+    //
+    // maybe just need to compute one way to do it. doesn't need to be perfect computation.
+    // - can group by origin query field, but how to know which one to reach? back to the steiner
+    // tree problem...
+    //
+    // for a single field -> it's the min of all resolvers/providable field cost to that field.
+    //  - from origin, find relevant query field, and move up in ancestry
+    // for multiple fields are the origin level, same thing.
+    // -> iterate recursively setting new origin and field.
+    //
+    // defining minimum cost of requirements is again a steiner tree problem!
+    // most common cases are only root fields that are easily computed though, so we can
+    // approximate that problem to the root fields.
+    //
+    // even for root fields, how to deal with it?
+    // iterate over all edges, set of edge cost id, different cases.
+    //
+    // Solve it the hard way, create all combinations. if parent can provide it, cost 0.
+    // otherwise combinations? yeah.
+    //
+    // edge -> impacted requirements computed from ancestry.
+    // ancestry not really useful otherwise with the steiner tree problem.
+    //
+    // requirements -> incoming edge cost.
+    //
+    // compute cost of requirement with very naive steiner tree algorithm.
+    //
     fn populate_from<Id>(&mut self, graph: &mut StableGraph<Node<Id>, Edge>) {
         let mut stack = Vec::new();
         let mut requirement_origins = Vec::new();
-        let mut origin_outgoing_edge_cost_ids = Vec::new();
         let mut ancestry_edges = Vec::new();
 
         // Initialize edge cost
@@ -105,25 +112,15 @@ impl CostEstimator {
                 self[*id] = 1;
             }
         }
-        let zero_edge_cost_id = EdgeCostId::from(self.edge_cost.len() - 1);
 
-        let zero_cost_ancestor_id = AncestorId::from(0usize);
-        self.all_ancestors.push(Ancestor {
-            // cycle with itself, but it's never part of any ancestry, so it's always only
-            // referenced as a parent.
-            parent: zero_cost_ancestor_id,
-            edge_cost_id: zero_edge_cost_id,
-            cumulative_cost_to_reach_requirement: 0,
-        });
-
-        for (query_field_ix, node) in graph.node_references() {
+        for (requirement_query_field_ix, node) in graph.node_references() {
             if !matches!(node, Node::QueryField(_)) {
                 continue;
             }
             debug_assert!(requirement_origins.is_empty());
             requirement_origins.extend(
                 graph
-                    .edges_directed(query_field_ix, Direction::Incoming)
+                    .edges_directed(requirement_query_field_ix, Direction::Incoming)
                     .filter_map(|edge| match edge.weight() {
                         Edge::Requires { origin_query_field_ix } => Some(*origin_query_field_ix),
                         _ => None,
@@ -143,31 +140,25 @@ impl CostEstimator {
             // resolves nested fields of the origin.
             let min_resolver_depth = min_origin_depth + 1;
 
-            let ancestry_start = self.all_ancestors.len();
             debug_assert!(stack.is_empty());
-            stack.extend(graph.edges_directed(query_field_ix, Direction::Incoming).filter_map(
-                |edge| match edge.weight() {
-                    Edge::Provides => Some((zero_cost_ancestor_id, 0, edge.source())),
-                    _ => None,
-                },
-            ));
+            stack.extend(
+                graph
+                    .edges_directed(requirement_query_field_ix, Direction::Incoming)
+                    .filter_map(|edge| match edge.weight() {
+                        Edge::Provides => Some(edge.source()),
+                        _ => None,
+                    }),
+            );
             debug_assert!(ancestry_edges.is_empty());
-            while let Some((parent_ancestor_id, parent_cost, node_ix)) = stack.pop() {
+            while let Some(node_ix) = stack.pop() {
                 for edge in graph.edges_directed(node_ix, Direction::Incoming) {
                     match edge.weight() {
-                        Edge::CreateChildResolver { id } | Edge::CanProvide { id } => {
-                            let cost = self[*id] + parent_cost;
-                            let ancestor_id = AncestorId::from(self.all_ancestors.len());
-                            self.all_ancestors.push(Ancestor {
-                                parent: parent_ancestor_id,
-                                edge_cost_id: *id,
-                                cumulative_cost_to_reach_requirement: cost,
-                            });
+                        Edge::CreateChildResolver { .. } | Edge::CanProvide { .. } => {
                             ancestry_edges.push(edge.id());
 
-                            let query_field_ix = edge.source();
-                            if graph[query_field_ix].query_depth() >= min_resolver_depth {
-                                stack.push((ancestor_id, cost, query_field_ix));
+                            let node_ix = edge.source();
+                            if graph[node_ix].query_depth() >= min_resolver_depth {
+                                stack.push(node_ix);
                             }
                         }
                         Edge::Provides
@@ -178,52 +169,14 @@ impl CostEstimator {
                     };
                 }
             }
-            let ancestry_end = self.all_ancestors.len();
 
-            let origin_root_ancestors_start = self.all_origin_root_ancestors.len();
-            requirement_origins.sort_unstable();
-            for origin_query_field_ix in requirement_origins.drain(..) {
-                origin_outgoing_edge_cost_ids.clear();
-                origin_outgoing_edge_cost_ids.extend(
-                    graph
-                        .edges_directed(origin_query_field_ix, Direction::Outgoing)
-                        .filter_map(|edge| match edge.weight() {
-                            Edge::CreateChildResolver { id } | Edge::CanProvide { id } => Some(*id),
-                            Edge::Field
-                            | Edge::TypenameField
-                            | Edge::HasChildResolver
-                            | Edge::Provides
-                            | Edge::Requires { .. } => None,
-                        }),
-                );
-                for (i, ancestor) in self.all_ancestors[ancestry_start..ancestry_end].iter().enumerate() {
-                    if origin_outgoing_edge_cost_ids.contains(&ancestor.edge_cost_id) {
-                        self.all_origin_root_ancestors.push(OriginRootAncestor {
-                            origin_query_field_ix,
-                            ancestor_id: AncestorId::from(ancestry_start + i),
-                        });
-                    }
-                }
-            }
-            let origin_root_ancestors_end = self.all_origin_root_ancestors.len();
-
-            let requirement_id = RequirementId::from(self.requirements.len());
-            self.requirements.push(Requirement {
-                query_field_ix,
-                ancestry_ids: IdRange::from(ancestry_start..ancestry_end),
-                origin_root_ancestor_ids: IdRange::from(origin_root_ancestors_start..origin_root_ancestors_end),
-            });
             ancestry_edges.sort_unstable();
-            self.edge_to_impacted_requirement
-                .extend(ancestry_edges.drain(..).dedup().map(|edge| (edge, requirement_id)));
-        }
-
-        for (i, requirement) in self.requirements.iter().enumerate() {
-            let id = RequirementId::from(i);
-            graph[requirement.query_field_ix]
-                .as_query_field_mut()
-                .unwrap()
-                .requirement_id = Some(id);
+            self.edge_to_impacted_requirement.extend(
+                ancestry_edges
+                    .drain(..)
+                    .dedup()
+                    .map(|edge| (edge, requirement_query_field_ix)),
+            );
         }
 
         self.edge_to_impacted_requirement.sort_unstable();
@@ -235,14 +188,14 @@ impl CostEstimator {
     fn fixed_point_iteration<Id>(
         &mut self,
         graph: &StableGraph<Node<Id>, Edge>,
-        mut impacted_requirements: Vec<RequirementId>,
+        mut impacted_requirements: Vec<NodeIndex>,
     ) -> crate::Result<()> {
         let mut iterations = 0;
         let mut impacted_nodes = impacted_requirements
             .drain(..)
-            .flat_map(|requirement_id| {
+            .flat_map(|requirement_query_field_ix| {
                 graph
-                    .edges_directed(self[requirement_id].query_field_ix, Direction::Incoming)
+                    .edges_directed(requirement_query_field_ix, Direction::Incoming)
                     .filter_map(|edge| match edge.weight() {
                         Edge::Requires { .. } => match graph[edge.source()] {
                             Node::Resolver(_) | Node::ProvidableField(_) => Some(edge.source()),
@@ -253,37 +206,34 @@ impl CostEstimator {
             })
             .collect::<Vec<_>>();
 
+        for node_ix in impacted_nodes.drain(..).dedup() {
+            let mut requirements = graph
+                .edges_directed(node_ix, Direction::Outgoing)
+                .filter_map(|edge| match edge.weight() {
+                    Edge::Requires { origin_query_field_ix } => Some((*origin_query_field_ix, edge.target())),
+                    _ => None,
+                })
+                .collect::<Vec<_>>();
+            requirements.sort_unstable();
+
+            for (origin_query_field_ix, requirement_query_field_indices) in
+                requirements.drain(..).dedup().chunk_by(|(ix, _)| *ix).into_iter()
+            {
+                todo!()
+            }
+        }
+
         while !impacted_nodes.is_empty() {
             debug_assert!(impacted_requirements.is_empty());
             impacted_nodes.sort_unstable();
             for node_ix in impacted_nodes.drain(..).dedup() {
-                // For now the cost of each incoming edge is assumed to be equal. Later we should
-                // take into account from which subgraph they come.
-                let cost = graph
+                let mut requirements = graph
                     .edges_directed(node_ix, Direction::Outgoing)
                     .filter_map(|edge| match edge.weight() {
-                        Edge::Requires { origin_query_field_ix } => Some((
-                            *origin_query_field_ix,
-                            graph[edge.target()].as_query_field().unwrap().requirement_id.unwrap(),
-                        )),
+                        Edge::Requires { origin_query_field_ix } => Some((*origin_query_field_ix, edge.target())),
                         _ => None,
                     })
-                    .map(|(origin_query_field_ix, requirement_id)| {
-                        self.requirement_cost(origin_query_field_ix, requirement_id)
-                    })
-                    .max()
-                    .unwrap_or_default();
-                for edge in graph.edges_directed(node_ix, Direction::Incoming) {
-                    let (id, cost) = match edge.weight() {
-                        Edge::CreateChildResolver { id } => (id, cost + 1),
-                        Edge::CanProvide { id } => (id, cost),
-                        _ => continue,
-                    };
-                    if self[*id] != cost {
-                        impacted_requirements.extend(self.impacted_requirements_for(edge.id()));
-                        self[*id] = cost;
-                    }
-                }
+                    .collect::<Vec<_>>();
             }
 
             debug_assert!(impacted_nodes.is_empty());
