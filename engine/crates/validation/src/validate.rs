b7a42499e1bc0d75450142e025cea70cc8531554
Tom Houlé <13155277+tomhoule@users.noreply.github.com>
2024-10-21 12:56:39 +0200

    validation: do not consider schema extensions as schema definitions (#2225)
    For the purpose of duplicate schema definition validation.
    
    closes GB-7760

--- engine/crates/validation/src/validate.rs
+++ engine/crates/validation/src/validate.rs
@@ -18,20 +18,35 @@ use self::{
     directive_definitions::*, enums::*, input_objects::*, input_types::*, interfaces::*, objects::*, scalars::*,
     schema_definition::*, type_definition::*, unions::*,
 };
-use crate::{diagnostics, Context, Options};
+use crate::{diagnostics, Context, Options, SchemaDefinition};
 use async_graphql_parser::{types as ast, Positioned};
 
 pub(crate) fn validate<'a>(parsed_ast: &'a ast::ServiceDocument, ctx: &mut Context<'a>) {
+    let mut schema_definitions = Vec::new();
+
     for definition in &parsed_ast.definitions {
         match definition {
-            ast::TypeSystemDefinition::Schema(def) => validate_schema_definition(def, ctx),
+            ast::TypeSystemDefinition::Schema(def) => {
+                schema_definitions.push(SchemaDefinition {
+                    pos: def.pos,
+                    directives: &def.node.directives,
+                    query: def.node.query.as_ref().map(|node| node.node.as_str()),
+                    mutation: def.node.mutation.as_ref().map(|node| node.node.as_str()),
+                    subscription: def.node.subscription.as_ref().map(|node| node.node.as_str()),
+                    is_extension: def.node.extend,
+                });
+            }
             ast::TypeSystemDefinition::Type(typedef) => validate_type_definition(typedef, ctx),
             ast::TypeSystemDefinition::Directive(def) => validate_directive_definition(def, ctx),
         }
     }
 
-    validate_schema_definition_references(ctx);
-    validate_root_types(ctx);
+    validate_schema_definitions(&schema_definitions, ctx);
+
+    if schema_definitions.is_empty() {
+        validate_root_types(ctx);
+    }
+
     validate_definitions_second_pass(parsed_ast, ctx);
 }
 
