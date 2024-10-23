d17d1758ec442c92cc51902f6194b96665ede39f
Julius de Bruijn <julius@nauk.io>
2024-10-23 10:21:54 +0200

    fix: no span for authenticate if there's no authorizers

--- engine/crates/gateway-v2/auth/src/lib.rs
+++ engine/crates/gateway-v2/auth/src/lib.rs
@@ -7,7 +7,7 @@ mod v1;
 use anonymous::AnonymousAuthorizer;
 use futures_util::{future::BoxFuture, stream::FuturesOrdered, StreamExt};
 use runtime::{auth::AccessToken, kv::KvStore, udf::AuthorizerInvoker};
-use tracing::instrument;
+use tracing::{info_span, Instrument};
 
 pub trait Authorizer: Send + Sync + 'static {
     fn get_access_token<'a>(&'a self, headers: &'a http::HeaderMap) -> BoxFuture<'a, Option<AccessToken>>;
@@ -16,24 +16,32 @@ pub trait Authorizer: Send + Sync + 'static {
 #[derive(Default)]
 pub struct AuthService {
     authorizers: Vec<Box<dyn Authorizer>>,
+    only_anonymous: bool,
 }
 
 impl AuthService {
     pub fn new(authorizers: Vec<Box<dyn Authorizer>>) -> Self {
-        Self { authorizers }
+        Self {
+            authorizers,
+            only_anonymous: false,
+        }
     }
 
     pub fn new_v1(config: config::v1::AuthConfig, kv: KvStore, udf_invoker: AuthorizerInvoker, ray_id: String) -> Self {
         Self {
             authorizers: vec![Box::new(v1::V1AuthProvider::new(ray_id, config, Some(kv), udf_invoker))],
+            only_anonymous: false,
         }
     }
 
     pub fn new_v2(config: config::v2::AuthConfig, kv: KvStore) -> Self {
-        let authorizers: Vec<Box<dyn Authorizer>> = if config.providers.is_empty() {
-            vec![Box::new(AnonymousAuthorizer)]
+        if config.providers.is_empty() {
+            Self {
+                authorizers: vec![Box::new(AnonymousAuthorizer)],
+                only_anonymous: true,
+            }
         } else {
-            config
+            let authorizers = config
                 .providers
                 .into_iter()
                 .map(|config| {
@@ -45,12 +53,15 @@ impl AuthService {
                     };
                     authorizer
                 })
-                .collect()
-        };
-        Self { authorizers }
+                .collect();
+
+            Self {
+                authorizers,
+                only_anonymous: false,
+            }
+        }
     }
 
-    #[instrument(skip_all)]
     pub async fn authenticate(&self, headers: &http::HeaderMap) -> Option<AccessToken> {
         let fut = self
             .authorizers
@@ -58,8 +69,15 @@ impl AuthService {
             .map(|authorizer| authorizer.get_access_token(headers))
             .collect::<FuturesOrdered<_>>()
             .filter_map(|token| async move { token });
+
         futures_util::pin_mut!(fut);
-        fut.next().await
+
+        if self.only_anonymous {
+            fut.next().await
+        } else {
+            let span = info_span!("authenticate");
+            fut.next().instrument(span).await
+        }
     }
 
     pub fn with_first_authorizer(mut self, authorizer: impl Authorizer) -> Self {
