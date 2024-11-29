7a98b89af90d546f2b411f14808be07941158f73
Yoav Lavi <yoav@yoavlavi.com>
2024-11-29 13:42:38 +0200

    unhide dev

--- cli/src/cli_input/sub_command.rs
+++ cli/src/cli_input/sub_command.rs
@@ -38,9 +38,7 @@ pub enum SubCommand {
     Upgrade,
     /// Lint a GraphQL schema
     Lint(LintCommand),
-    // hidden for internal beta
     /// Start a development server
-    #[clap(hide = true)]
     Dev(DevCommand),
 }
 
