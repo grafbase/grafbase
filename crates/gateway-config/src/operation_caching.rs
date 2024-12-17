55525fb30aa8af19e4292b402cff3130b6e1f5f4
Graeme Coupar <graeme@turbofish.tech>
2024-12-17 14:44:23 +0000

    simple af redis tiered caching impl

--- crates/gateway-config/src/operation_caching.rs
+++ crates/gateway-config/src/operation_caching.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 #[derive(Debug, Clone, PartialEq, serde::Deserialize)]
 #[serde(default, deny_unknown_fields)]
 pub struct OperationCaching {
@@ -10,6 +12,10 @@ pub struct OperationCaching {
     pub warm_on_reload: bool,
     /// The percentage of the cache that will be warmed on reload
     pub warming_percent: u8,
+
+    /// Configuration for a redis server that will be used as a fallback if
+    /// in memory cache misses
+    pub redis: Option<OperationCachingRedisConfig>,
 }
 
 impl Default for OperationCaching {
@@ -19,6 +25,33 @@ impl Default for OperationCaching {
             limit: 1000,
             warm_on_reload: false,
             warming_percent: 100,
+            redis: None,
         }
     }
+}
+
+#[derive(Debug, Clone, PartialEq, serde::Deserialize)]
+#[serde(default, deny_unknown_fields)]
+pub struct OperationCachingRedisConfig {
+    pub url: url::Url,
+    pub key_prefix: String,
+    pub tls: Option<OperationCachingRedisTlsConfig>,
+}
+
+impl Default for OperationCachingRedisConfig {
+    fn default() -> Self {
+        Self {
+            url: url::Url::parse("redis://localhost:6379").expect("must be correct"),
+            key_prefix: String::from("grafbase-operation-cache"),
+            tls: None,
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Default, serde::Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct OperationCachingRedisTlsConfig {
+    pub cert: Option<PathBuf>,
+    pub key: Option<PathBuf>,
+    pub ca: Option<PathBuf>,
 }
---
+++ crates/runtime-local/src/operation_cache/redis.rs
@@ -0,0 +1,66 @@
+use redis::{AsyncCommands, RedisResult};
+use runtime::operation_cache::OperationCache;
+
+use crate::redis::Pool;
+
+#[derive(Clone)]
+pub struct RedisOperationCache {
+    pool: Pool,
+    key_prefix: String,
+}
+
+impl RedisOperationCache {
+    pub fn new(pool: Pool, key_prefix: &str) -> Self {
+        RedisOperationCache {
+            pool,
+            key_prefix: key_prefix.to_string(),
+        }
+    }
+}
+
+impl<V> OperationCache<V> for RedisOperationCache
+where
+    V: Clone + Send + Sync + 'static + serde::Serialize + serde::de::DeserializeOwned,
+{
+    async fn insert(&self, key: String, value: V) {
+        let Ok(value) = serde_json::to_vec(&value) else { return };
+        let Ok(mut connection) = self.pool.get().await else {
+            return;
+        };
+
+        let key = format!("{}{key}", self.key_prefix);
+
+        let result: RedisResult<()> = connection.set(&key, &value).await;
+        if let Err(err) = result {
+            tracing::warn!("could not decode the data stored in key {key} from redis operation cache: {err}");
+        }
+    }
+
+    async fn get(&self, key: &String) -> Option<V> {
+        let Ok(mut connection) = self.pool.get().await else {
+            return None;
+        };
+
+        let key = format!("{}{key}", self.key_prefix);
+
+        let result: RedisResult<Option<Vec<u8>>> = connection.get(&key).await;
+        let bytes = match result {
+            Ok(None) => {
+                return None;
+            }
+            Ok(Some(bytes)) => bytes,
+            Err(e) => {
+                tracing::warn!("could not fetch the key {key} from redis operation cache: {e}");
+                return None;
+            }
+        };
+
+        match serde_json::from_slice(&bytes) {
+            Ok(value) => Some(value),
+            Err(err) => {
+                tracing::warn!("could not decode the data stored in key {key} from redis operation cache: {err}");
+                None
+            }
+        }
+    }
+}
