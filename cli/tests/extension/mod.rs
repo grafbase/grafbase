7fbe750eddf2d7e5f998fcf8fc91483131ea286f
Julius de Bruijn <julius@nauk.io>
2025-02-07 11:18:04 +0000

    chore: document sdk test utils, document all features on docs.rs

--- cli/tests/extension/mod.rs
+++ cli/tests/extension/mod.rs
@@ -35,12 +35,12 @@ fn init_resolver() {
     codegen-units = 1
 
     [dependencies]
-    grafbase-sdk = "0.1.6"
+    grafbase-sdk = "0.1.7"
 
     [dev-dependencies]
     indoc = "2"
     insta = { version = "1.42.1", features = ["json"] }
-    grafbase-sdk = { version = "0.1.6", features = ["test-utils"] }
+    grafbase-sdk = { version = "0.1.7", features = ["test-utils"] }
     tokio = { version = "1", features = ["rt-multi-thread", "macros", "test-util"] }
     serde_json = "1"
     "#);
@@ -253,12 +253,12 @@ fn init_auth() {
     codegen-units = 1
 
     [dependencies]
-    grafbase-sdk = "0.1.6"
+    grafbase-sdk = "0.1.7"
 
     [dev-dependencies]
     indoc = "2"
     insta = { version = "1.42.1", features = ["json"] }
-    grafbase-sdk = { version = "0.1.6", features = ["test-utils"] }
+    grafbase-sdk = { version = "0.1.7", features = ["test-utils"] }
     tokio = { version = "1", features = ["rt-multi-thread", "macros", "test-util"] }
     serde_json = "1"
     "#);
