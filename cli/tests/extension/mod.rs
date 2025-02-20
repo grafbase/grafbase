0f0f90e3c6df4ac686cd61107d25e95ffade73a5
Julius de Bruijn <julius@nauk.io>
2025-02-20 19:22:24 +0000

    fix: update grafbase-sdk

--- cli/tests/extension/mod.rs
+++ cli/tests/extension/mod.rs
@@ -35,12 +35,12 @@ fn init_resolver() {
     codegen-units = 1
 
     [dependencies]
-    grafbase-sdk = "0.1.15"
+    grafbase-sdk = "0.1.16"
 
     [dev-dependencies]
     indoc = "2"
     insta = { version = "1.42.1", features = ["json"] }
-    grafbase-sdk = { version = "0.1.15", features = ["test-utils"] }
+    grafbase-sdk = { version = "0.1.16", features = ["test-utils"] }
     tokio = { version = "1", features = ["rt-multi-thread", "macros", "test-util"] }
     serde_json = "1"
     "#);
@@ -263,12 +263,12 @@ fn init_auth() {
     codegen-units = 1
 
     [dependencies]
-    grafbase-sdk = "0.1.15"
+    grafbase-sdk = "0.1.16"
 
     [dev-dependencies]
     indoc = "2"
     insta = { version = "1.42.1", features = ["json"] }
-    grafbase-sdk = { version = "0.1.15", features = ["test-utils"] }
+    grafbase-sdk = { version = "0.1.16", features = ["test-utils"] }
     tokio = { version = "1", features = ["rt-multi-thread", "macros", "test-util"] }
     serde_json = "1"
     "#);
