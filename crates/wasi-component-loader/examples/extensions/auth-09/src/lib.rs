5c0426cf10f5bf1bda36f6c0275454f8514377d8
Julius de Bruijn <julius@nauk.io>
2025-03-13 06:56:33 +0000

    chore: add a few smoke tests for older sdk versions

---
+++ crates/wasi-component-loader/examples/extensions/auth-09/src/lib.rs
@@ -0,0 +1,52 @@
+use std::time::Duration;
+
+use grafbase_sdk::{
+    AuthenticationExtension, Error, Headers,
+    host_io::cache::{self, CachedItem},
+    types::{Configuration, ErrorResponse, StatusCode, Token},
+};
+
+#[derive(AuthenticationExtension)]
+struct CachingProvider {
+    config: ProviderConfig,
+}
+
+#[derive(Debug, serde::Deserialize)]
+struct ProviderConfig {
+    cache_config: String,
+}
+
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
+struct Jwks {
+    key: String,
+}
+
+impl AuthenticationExtension for CachingProvider {
+    fn new(config: Configuration) -> Result<Self, Error> {
+        let config: ProviderConfig = config.deserialize()?;
+
+        Ok(Self { config })
+    }
+
+    fn authenticate(&mut self, headers: Headers) -> Result<Token, ErrorResponse> {
+        let header = headers
+            .get("Authorization")
+            .ok_or_else(|| ErrorResponse::new(StatusCode::UNAUTHORIZED))?;
+
+        let value = headers.get("value").unwrap_or_else(|| String::from("default"));
+
+        let cache_key = format!("auth:{}:{header}", self.config.cache_config);
+
+        let jwks: Jwks = cache::get(&cache_key, || {
+            std::thread::sleep(Duration::from_millis(300));
+
+            let jwks = Jwks { key: value };
+            let item = CachedItem::new(jwks, None);
+
+            Ok(item)
+        })
+        .unwrap();
+
+        Ok(Token::from_bytes(serde_json::to_vec(&jwks).unwrap()))
+    }
+}
---
+++ crates/wasi-component-loader/examples/extensions/resolver-08/src/lib.rs
@@ -0,0 +1,73 @@
+use grafbase_sdk::{
+    Error, Extension, Headers, Resolver, ResolverExtension, Subscription,
+    types::{Configuration, FieldDefinitionDirective, FieldInputs, FieldOutput},
+};
+
+#[derive(ResolverExtension)]
+struct SimpleResolver {
+    schema_args: SchemaArgs,
+}
+
+#[derive(serde::Deserialize)]
+struct SchemaArgs {
+    id: usize,
+}
+
+#[derive(serde::Deserialize)]
+struct FieldArgs<'a> {
+    name: &'a str,
+}
+
+#[derive(serde::Serialize)]
+struct ResponseOutput<'a> {
+    id: usize,
+    name: &'a str,
+}
+
+impl Extension for SimpleResolver {
+    fn new(
+        schema_directives: Vec<grafbase_sdk::types::SchemaDirective>,
+        _: Configuration,
+    ) -> Result<Self, Box<dyn std::error::Error>>
+    where
+        Self: Sized,
+    {
+        let schema_args = schema_directives
+            .into_iter()
+            .filter(|d| d.name() == "schemaArgs")
+            .map(|d| d.arguments().unwrap())
+            .next()
+            .unwrap();
+
+        Ok(Self { schema_args })
+    }
+}
+
+impl Resolver for SimpleResolver {
+    fn resolve_field(
+        &mut self,
+        _: Headers,
+        _: &str,
+        directive: FieldDefinitionDirective,
+        _: FieldInputs,
+    ) -> Result<FieldOutput, Error> {
+        let args: FieldArgs = directive.arguments().unwrap();
+        let mut output = FieldOutput::new();
+
+        output.push_value(ResponseOutput {
+            id: self.schema_args.id,
+            name: args.name,
+        });
+
+        Ok(output)
+    }
+
+    fn resolve_subscription(
+        &mut self,
+        _: Headers,
+        _: &str,
+        _: FieldDefinitionDirective,
+    ) -> Result<Box<dyn Subscription>, Error> {
+        todo!()
+    }
+}
