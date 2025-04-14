98b02c8d939d99d127ce1db09015772ee2e645a8
Tom Houlé <13155277+tomhoule@users.noreply.github.com>
2025-04-14 17:54:49 +0200

    protoc-gen-grafbase-subgraph: well-known types support (#3025)
    Not _everything_, but the types we support in the extension so far.
    
    See [the ProtoJSON format
    docs](https://protobuf.dev/programming-guides/json/) for the canonical
    mapping to JSON.
    
    Also fix the field types of input types being rendered as output types.
    
    closes GB-8845

--- crates/protoc-gen-grafbase-subgraph/src/render_graphql_sdl/graphql_types.rs
+++ crates/protoc-gen-grafbase-subgraph/src/render_graphql_sdl/graphql_types.rs
@@ -1,3 +1,5 @@
+use crate::schema::ScalarType;
+
 use super::*;
 
 pub(super) fn render_graphql_types(
@@ -37,6 +39,25 @@ fn render_message(
 ) -> fmt::Result {
     let message = schema.view(message_id);
 
+    match message.name.as_str() {
+        ".google.protobuf.Duration"
+        | ".google.protobuf.Timestamp"
+        | ".google.protobuf.FieldMask"
+        | ".google.protobuf.BoolValue"
+        | ".google.protobuf.BytesValue"
+        | ".google.protobuf.DoubleValue"
+        | ".google.protobuf.FloatValue"
+        | ".google.protobuf.Int32Value"
+        | ".google.protobuf.Int64Value"
+        | ".google.protobuf.StringValue"
+        | ".google.protobuf.UInt32Value"
+        | ".google.protobuf.UInt64Value" => return Ok(()),
+
+        ".google.protobuf.Empty" => f.write_str("\n\"An empty object \" scalar EmptyObject\n")?,
+
+        _ => (),
+    }
+
     f.write_str("\n")?;
 
     if let Some(description) = message.description.as_deref() {
@@ -57,7 +78,13 @@ fn render_message(
         f.write_str(INDENT)?;
         f.write_str(&field.name)?;
         f.write_str(": ")?;
-        render_output_field_type(schema, &field.r#type, field.repeated, f)?;
+
+        if input {
+            render_input_field_type(schema, &field.r#type, field.repeated, f)?;
+        } else {
+            render_output_field_type(schema, &field.r#type, field.repeated, f)?;
+        }
+
         f.write_str("\n")?;
     }
 
@@ -76,7 +103,7 @@ pub(super) fn render_output_field_type(
 
     match ty {
         FieldType::Scalar(scalar_type) => scalar_type.render_graphql_type(f)?,
-        FieldType::Message(proto_message_id) => schema.view(*proto_message_id).graphql_output_name().fmt(f)?,
+        FieldType::Message(proto_message_id) => render_message_type_name(schema, *proto_message_id, false, f)?,
         FieldType::Enum(proto_enum_id) => schema.view(*proto_enum_id).graphql_name().fmt(f)?,
     }
 
@@ -99,7 +126,7 @@ pub(super) fn render_input_field_type(
 
     match ty {
         FieldType::Scalar(scalar_type) => scalar_type.render_graphql_type(f)?,
-        FieldType::Message(proto_message_id) => schema.view(*proto_message_id).graphql_input_name().fmt(f)?,
+        FieldType::Message(proto_message_id) => render_message_type_name(schema, *proto_message_id, true, f)?,
         FieldType::Enum(proto_enum_id) => schema.view(*proto_enum_id).graphql_name().fmt(f)?,
     }
 
@@ -136,4 +163,38 @@ fn render_enum_definition(schema: &GrpcSchema, enum_id: ProtoEnumId, f: &mut fmt
 
 pub(super) fn render_description(f: &mut fmt::Formatter<'_>, description: &str) -> fmt::Result {
     writeln!(f, "\"\"\"\n{description}\n\"\"\"", description = description.trim())
+}
+
+fn render_message_type_name(
+    schema: &GrpcSchema,
+    proto_message_id: ProtoMessageId,
+    is_input: bool,
+    f: &mut fmt::Formatter<'_>,
+) -> fmt::Result {
+    let message = schema.view(proto_message_id);
+
+    // See the docs for the mapping of well-known types: https://protobuf.dev/programming-guides/json/
+    match message.name.as_str() {
+        ".google.protobuf.Duration" => f.write_str("String"),
+        ".google.protobuf.Timestamp" => f.write_str("String"),
+        ".google.protobuf.FieldMask" => f.write_str("String"),
+        ".google.protobuf.Empty" => f.write_str("EmptyObject"),
+        ".google.protobuf.BoolValue" => ScalarType::Bool.render_graphql_type(f),
+        ".google.protobuf.BytesValue" => ScalarType::Bytes.render_graphql_type(f),
+        ".google.protobuf.DoubleValue" => ScalarType::Double.render_graphql_type(f),
+        ".google.protobuf.FloatValue" => ScalarType::Float.render_graphql_type(f),
+        ".google.protobuf.Int32Value" => ScalarType::Int32.render_graphql_type(f),
+        ".google.protobuf.Int64Value" => ScalarType::Int64.render_graphql_type(f),
+        ".google.protobuf.StringValue" => ScalarType::String.render_graphql_type(f),
+        ".google.protobuf.UInt32Value" => ScalarType::UInt32.render_graphql_type(f),
+        ".google.protobuf.UInt64Value" => ScalarType::UInt64.render_graphql_type(f),
+
+        _ => {
+            if is_input {
+                message.graphql_input_name().fmt(f)
+            } else {
+                message.graphql_output_name().fmt(f)
+            }
+        }
+    }
 }
