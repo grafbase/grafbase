b0a461d685f1efb546b7e6b0919eb55bdbe6a030
Tom Houlé <tom@tomhoule.com>
2025-04-15 09:49:43 +0200

    (wip) gRPC example

---
+++ examples/grpc-extension/server/src/data.rs
@@ -0,0 +1,32 @@
+use serde::Deserialize;
+use std::fs::File;
+
+#[derive(Debug, Deserialize)]
+struct Feature {
+    location: Location,
+    name: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Location {
+    latitude: i32,
+    longitude: i32,
+}
+
+pub fn load() -> Vec<crate::routeguide::Feature> {
+    let data_file_path = std::path::Path::new("db.json");
+    let file = File::open(data_file_path).expect("failed to open data file");
+
+    let decoded: Vec<Feature> = serde_json::from_reader(&file).expect("failed to deserialize features");
+
+    decoded
+        .into_iter()
+        .map(|feature| crate::routeguide::Feature {
+            name: feature.name,
+            location: Some(crate::routeguide::Point {
+                longitude: feature.location.longitude,
+                latitude: feature.location.latitude,
+            }),
+        })
+        .collect()
+}
