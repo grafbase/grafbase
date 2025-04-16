b18be2b5b3dd9b778c2af20f3986f711334bc338
Tom Houlé <13155277+tomhoule@users.noreply.github.com>
2025-04-16 21:25:59 +0200

    gRPC example (#3047)
    See the README in the PR for details.
    
    closes GB-8852

---
+++ examples/grpc-extension/server/build.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let proto_path = concat!(env!("CARGO_MANIFEST_DIR"), "/../proto");
+
+    tonic_build::configure()
+        .type_attribute("routeguide.Point", "#[derive(Hash)]")
+        .compile_protos(&[&format!("{proto_path}/route_guide.proto")], &[proto_path])
+        .unwrap();
+}
