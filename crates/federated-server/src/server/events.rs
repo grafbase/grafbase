b0c516911e2e71c9168aadeaa18f42e64708b03e
Julius de Bruijn <julius@nauk.io>
2025-07-07 07:37:43 +0000

    chore: review fixes

--- crates/federated-server/src/server/events.rs
+++ crates/federated-server/src/server/events.rs
@@ -12,45 +12,6 @@ pub enum UpdateEvent {
     Config(Box<Config>),
 }
 
-#[allow(dead_code)]
-impl UpdateEvent {
-    /// Creates a new graph update event
-    pub fn graph(definition: GraphDefinition) -> Self {
-        Self::Graph(definition)
-    }
-
-    /// Creates a new config update event
-    pub fn config(config: Config) -> Self {
-        Self::Config(Box::new(config))
-    }
-
-    /// Returns true if this is a graph update event
-    pub fn is_graph(&self) -> bool {
-        matches!(self, Self::Graph(_))
-    }
-
-    /// Returns true if this is a config update event
-    pub fn is_config(&self) -> bool {
-        matches!(self, Self::Config(_))
-    }
-
-    /// Returns the graph definition if this is a graph update event
-    pub fn as_graph(&self) -> Option<&GraphDefinition> {
-        match self {
-            Self::Graph(def) => Some(def),
-            _ => None,
-        }
-    }
-
-    /// Returns the config if this is a config update event
-    pub fn as_config(&self) -> Option<&Config> {
-        match self {
-            Self::Config(config) => Some(config),
-            _ => None,
-        }
-    }
-}
-
 impl std::fmt::Display for UpdateEvent {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
