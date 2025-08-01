39750249115b6d8d7dfaf3e8191a20222a008f0e
Benjamin Rabier <benjamin+git@rabier.dev>
2025-08-01 22:36:46 +0200

    Remove legacy examples & flake.nix

--- examples/gateway-hooks/hooks/src/gateway.rs
+++
@@ -1,9 +0,0 @@
-use crate::{
-    bindings::{
-        component::grafbase::types::{Context, ErrorResponse, Headers},
-        exports::component::grafbase::gateway_request,
-    },
-    init_logging, Component,
-};
-
-impl gateway_request::Guest for Component {}
