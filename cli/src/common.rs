cdbfd671e9f19ce77c51d9405814579e3a335168
Tom Houlé <tom@tomhoule.com>
2025-08-07 15:12:07 +0200

    cli: output pretty logs by default
    Like the gateway --log-style=pretty. It's more readable.
    
    closes GB-9483

--- cli/src/common.rs
+++ cli/src/common.rs
@@ -6,6 +6,7 @@ pub(crate) mod consts;
 pub(crate) mod debug_macros;
 pub(crate) mod environment;
 pub(crate) mod errors;
+pub(crate) mod log;
 pub(crate) mod trusted_documents;
 pub(crate) mod types;
 pub(crate) mod utils;
