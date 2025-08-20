f0af057b9c5dc5a89c745184f7b1b731c9c752a0
Tom Houlé <tom@tomhoule.com>
2025-08-20 10:46:51 +0200

    graphql-composition: remove LoadedExtension.version
    It isn't used anywhere.

--- crates/graphql-composition/src/grafbase_extensions.rs
+++ crates/graphql-composition/src/grafbase_extensions.rs
@@ -3,10 +3,8 @@
 pub struct LoadedExtension {
     /// The URL of the extension, which can be a remote URL or a local file path.
     pub link_url: String,
-    /// URL to use in the federated SDL
+    /// URL to use in the federated SDL.
     pub url: url::Url,
     /// The unique name of the extension.
     pub name: String,
-    /// The version of the extension.
-    pub version: String,
 }
