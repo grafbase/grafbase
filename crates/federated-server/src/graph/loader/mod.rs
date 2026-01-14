a57920cd6b3e29970cf7b1317038b9d1b9bc8831
Arda TANRIKULU <ardatanrikulu@gmail.com>
2026-01-14 22:06:04 +0300

    Support fetching Supergraph from Hive Console CDN

--- crates/federated-server/src/graph/loader/mod.rs
+++ crates/federated-server/src/graph/loader/mod.rs
@@ -1,9 +1,10 @@
 mod object_storage;
 mod schema_file;
 
-use std::path::PathBuf;
+use std::{path::PathBuf, time::Duration};
 
 use graph_ref::GraphRef;
+use hive_console_sdk::supergraph_fetcher::SupergraphFetcher;
 use tokio::sync::mpsc;
 
 pub use object_storage::*;
@@ -26,6 +27,11 @@ pub enum GraphLoader {
     },
     FromChannel {
         sdl_receiver: mpsc::Receiver<String>,
+    },
+    FromHiveConsole {
+        endpoints: Vec<String>,
+        key: Option<String>,
+        poll_interval: Duration,
     },
 }
 
@@ -78,6 +84,45 @@ impl GraphLoader {
                         if sender.send(UpdateEvent::Graph(Graph::FromText { sdl })).await.is_err() {
                             break;
                         }
+                    }
+                });
+
+                Ok(())
+            }
+            GraphLoader::FromHiveConsole {
+                endpoints,
+                key,
+                poll_interval,
+            } => {
+                let mut builder = SupergraphFetcher::builder().user_agent("grafbase".to_string());
+                for endpoint in endpoints {
+                    builder = builder.add_endpoint(endpoint);
+                }
+                if let Some(key) = key {
+                    builder = builder.key(key);
+                }
+                let fetcher = builder
+                    .build_async()
+                    .map_err(|e| crate::Error::InternalError(format!("Failed to create SupergraphFetcher: {}", e)))?;
+                tokio::spawn(async move {
+                    let mut interval = tokio::time::interval(poll_interval);
+
+                    loop {
+                        match fetcher.fetch_supergraph().await {
+                            Ok(Some(sdl)) => {
+                                if sender.send(UpdateEvent::Graph(Graph::FromText { sdl })).await.is_err() {
+                                    break;
+                                }
+                            }
+                            Ok(None) => {
+                                // No update available
+                            }
+                            Err(e) => {
+                                tracing::error!("Error fetching supergraph SDL: {}", e);
+                            }
+                        };
+
+                        interval.tick().await;
                     }
                 });
 
